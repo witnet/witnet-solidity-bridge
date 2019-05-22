632c0c82a93c6e24041aa4a1791d016c820fd33e
Gorka Irazoqui <gorka.irazoki@gmail.com>
2019-05-22 12:58:04 +0200

    feat: add report and read result
    Co-authored-by: Mario Cao <mariocao@gmail.com>

--- contracts/WitnetBridgeInterface.sol
+++ contracts/WitnetBridgeInterface.sol
@@ -5,6 +5,7 @@ contract WitnetBridgeInterface {
     struct DataRequest {
         bytes script;
         bytes result;
+        uint256 reward;
     }
 
     mapping (uint256 => DataRequest) public requests;
@@ -13,10 +14,11 @@ contract WitnetBridgeInterface {
     {
     }
 
-    function post_dr(bytes memory dr, uint256 post_reward, uint256 report_reward) public returns(uint256 id) {
+    function post_dr(bytes memory dr, uint256 reward) public returns(uint256 id) {
         id = uint256(keccak256(dr));
         requests[id].script = dr;
         requests[id].result = "";
+        requests[id].reward = reward;
         return id;
     }
 
@@ -24,14 +26,11 @@ contract WitnetBridgeInterface {
         return requests[id].script;
     }
 
-    /*function upgrade_reward(uint256 id, uint256 post_reward, uint256 report_reward) public {
+    function report_result (uint256 id, bytes memory result) public {
+        requests[id].result = result;       
     }
 
-    function claim_drs(uint256[] memory ids, bytes memory poe) public {}
-
-    function claim_post_reward(uint256 id, bytes memory poi, uint256 block_number) public {}
-
-    function report_result (uint256 id, bytes memory poi, bytes memory poe, uint256 block_number, bytes memory result) public {}
-
-    function read_result (uint256 id) public view {}*/
+    function read_result (uint256 id) public view returns(bytes memory result){
+        return requests[id].result;
+    }
 }
--- test/wbi.js
+++ test/wbi.js
@@ -7,23 +7,44 @@ contract("WBI", _accounts => {
       wbiInstance = await WBI.deployed()
     })
 
-    it("should allow post and read", async () => {
+    it("should allow post and read drs", async () => {
       const waitForHash = (txQ) => new Promise((resolve, reject) => txQ.on('transactionHash', hash => resolve(hash)).catch(reject))
 
       const drBytes = web3.utils.fromAscii("This is a DR?")
-      // console.log(`Data request bytes: ${drBytes}`)
+      console.log(`Data request bytes: ${drBytes}`)
 
       // Get id the easy way
-      const idComputed = await wbiInstance.post_dr.call(drBytes, 1, 1)
+      const idComputed = await wbiInstance.post_dr.call(drBytes, 1)
       // console.log(`Id computed: ${idComputed}`)
 
-      const tx = wbiInstance.post_dr(drBytes, 1, 1)
+      const tx = wbiInstance.post_dr(drBytes, 1)
       await waitForHash(tx)
 
       let readDrBytes = await wbiInstance.read_dr.call(idComputed)
       // console.log(`Data request bytes read from Contract: ${readDrBytes}`)
 
       assert.equal(drBytes, readDrBytes)
-    })
+    });
+    it("should allow post and read result", async () => {
+      const waitForHash = (txQ) => new Promise((resolve, reject) => txQ.on('transactionHash', hash => resolve(hash)).catch(reject))
+
+      const drBytes = web3.utils.fromAscii("This is a DR?")
+      const resBytes = web3.utils.fromAscii("This is a result?")
+
+      // Get id the easy way
+      const idComputed = await wbiInstance.post_dr.call(drBytes, 1)
+      // console.log(`Id computed: ${idComputed}`)
+
+      const tx = wbiInstance.post_dr(drBytes, 1)
+      await waitForHash(tx)
+
+      // report result
+      let restx = wbiInstance.report_result(idComputed, resBytes)
+      await waitForHash(restx)
+
+      let readResBytes = await wbiInstance.read_result.call(idComputed)
+
+      assert.equal(resBytes, readResBytes)
+    });
   })
 })
