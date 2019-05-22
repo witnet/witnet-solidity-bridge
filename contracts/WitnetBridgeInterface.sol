042ed0860bbb19d5ba01d0714df48ba54133270a
Gorka Irazoqui <gorka.irazoki@gmail.com>
2019-05-22 18:06:51 +0200

    feat: add escrow and payment
    Co-authored-by: Mario Cao <mariocao@gmail.com>

--- contracts/WitnetBridgeInterface.sol
+++ contracts/WitnetBridgeInterface.sol
@@ -1,36 +1,40 @@
 pragma solidity ^0.5.0;
 
 contract WitnetBridgeInterface {
+  
+  struct DataRequest {
+    bytes script;
+    bytes result;
+    uint256 reward;
+  }
 
-    struct DataRequest {
-        bytes script;
-        bytes result;
-        uint256 reward;
-    }
+  uint256 counter;
+  mapping (uint256 => DataRequest) public requests;
 
-    mapping (uint256 => DataRequest) public requests;
+  constructor () public
+  {
+    counter = 0;
+  }
 
-    constructor () public
-    {
-    }
+  function post_dr(bytes memory dr) public payable returns(uint256 id) {
+    id = counter;
+    counter++;
+    requests[id].script = dr;
+    requests[id].result = "";
+    requests[id].reward = msg.value;
+    return id;
+  }
 
-    function post_dr(bytes memory dr, uint256 reward) public returns(uint256 id) {
-        id = uint256(keccak256(dr));
-        requests[id].script = dr;
-        requests[id].result = "";
-        requests[id].reward = reward;
-        return id;
-    }
+  function read_dr(uint256 id) public view returns(bytes memory dr) {
+    return requests[id].script;
+  }
 
-    function read_dr(uint256 id) public view returns(bytes memory dr) {
-        return requests[id].script;
-    }
+  function report_result (uint256 id, bytes memory result) public {
+    requests[id].result = result;
+    msg.sender.transfer(requests[id].reward);
+  }
 
-    function report_result (uint256 id, bytes memory result) public {
-        requests[id].result = result;       
-    }
-
-    function read_result (uint256 id) public view returns(bytes memory result){
-        return requests[id].result;
-    }
+  function read_result (uint256 id) public view returns(bytes memory result){
+    return requests[id].result;
+  }
 }
--- test/wbi.js
+++ test/wbi.js
@@ -1,50 +1,68 @@
 const WBI = artifacts.require("WitnetBridgeInterface")
 
-contract("WBI", _accounts => {
+contract("WBI", accounts => {
   describe("WBI test suite", () => {
     let wbiInstance
-    before(async () => {
-      wbiInstance = await WBI.deployed()
+    beforeEach(async () => {
+      wbiInstance = await WBI.new()
     })
 
     it("should allow post and read drs", async () => {
-      const waitForHash = (txQ) => new Promise((resolve, reject) => txQ.on('transactionHash', hash => resolve(hash)).catch(reject))
+      const waitForHash = (txQ) => new Promise((resolve, reject) => txQ.on("transactionHash", hash => resolve(hash)).catch(reject))
+      // Take current balance
+      var account1 = accounts[0]
+      let actualBalance1 = await web3.eth.getBalance(account1)
 
       const drBytes = web3.utils.fromAscii("This is a DR?")
-      console.log(`Data request bytes: ${drBytes}`)
+      const drBytes2 = web3.utils.fromAscii("This is a second DR?")
+      let contract_balance = await web3.eth.getBalance(wbiInstance.address)
 
-      // Get id the easy way
-      const idComputed = await wbiInstance.post_dr.call(drBytes, 1)
-      // console.log(`Id computed: ${idComputed}`)
-
-      const tx = wbiInstance.post_dr(drBytes, 1)
+      const tx = wbiInstance.post_dr(drBytes, { from: account1, value: web3.utils.toWei("1", "ether") })
       await waitForHash(tx)
+      const tx2 = wbiInstance.post_dr(drBytes2)
+      await waitForHash(tx2)
 
-      let readDrBytes = await wbiInstance.read_dr.call(idComputed)
-      // console.log(`Data request bytes read from Contract: ${readDrBytes}`)
+      let readDrBytes = await wbiInstance.read_dr.call(0)
+      let readDrBytes2 = await wbiInstance.read_dr.call(1)
+
+      let afterBalance1 = await web3.eth.getBalance(account1)
+      let contract_balance_after = await web3.eth.getBalance(wbiInstance.address)
+
+      assert(afterBalance1 < actualBalance1)
+      assert.equal(web3.utils.toWei("1", "ether"), contract_balance_after)
 
       assert.equal(drBytes, readDrBytes)
-    });
+      assert.equal(drBytes2, readDrBytes2)
+    })
     it("should allow post and read result", async () => {
-      const waitForHash = (txQ) => new Promise((resolve, reject) => txQ.on('transactionHash', hash => resolve(hash)).catch(reject))
+      var account1 = accounts[0]
+      var account2 = accounts[1]
+      let actualBalance1 = await web3.eth.getBalance(account1)
+      let actualBalance2 = await web3.eth.getBalance(account2)
+
+      const waitForHash = (txQ) => new Promise((resolve, reject) => txQ.on("transactionHash", hash => resolve(hash)).catch(reject))
 
       const drBytes = web3.utils.fromAscii("This is a DR?")
       const resBytes = web3.utils.fromAscii("This is a result?")
 
-      // Get id the easy way
-      const idComputed = await wbiInstance.post_dr.call(drBytes, 1)
-      // console.log(`Id computed: ${idComputed}`)
-
-      const tx = wbiInstance.post_dr(drBytes, 1)
+      const tx = wbiInstance.post_dr(drBytes, { from: account1, value: web3.utils.toWei("1", "ether") })
       await waitForHash(tx)
 
       // report result
-      let restx = wbiInstance.report_result(idComputed, resBytes)
+      let restx = wbiInstance.report_result(0, resBytes, { from: account2 })
       await waitForHash(restx)
 
-      let readResBytes = await wbiInstance.read_result.call(idComputed)
+      let afterBalance1 = await web3.eth.getBalance(account1)
+      let afterBalance2 = await web3.eth.getBalance(account2)
+
+      let contract_balance_after = await web3.eth.getBalance(wbiInstance.address)
+
+      assert(afterBalance1 < actualBalance1)
+      assert(afterBalance2 > actualBalance2)
+      assert.equal(0, contract_balance_after)
+      let readResBytes = await wbiInstance.read_result.call(0)
 
       assert.equal(resBytes, readResBytes)
-    });
+    })
   })
 })
