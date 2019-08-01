9f1d85c0dc624fe51815fb56de9664e8c6968873
Luis Rubio <luisr@stampery.co>
2019-08-01 12:24:27 +0200

    feat: add local and ropsten networks to connect with bridge

--- truffle-config.js
+++ truffle-config.js
@@ -83,6 +83,21 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    local: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+
+    ropsten: {
+      network_id: 3,       // Ropsten's id
+      host: "127.0.0.1",   // Localhost (default: none)
+      port: 8545,          // Standard Ethereum port (default: none)
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
