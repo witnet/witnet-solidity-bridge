735ba4e50371df12eb2bdc158bfb8d1ad476bf49
Adán SDPC <adan.sdpc@gmail.com>
2019-08-07 12:54:43 +0200

    docs(UsingWitnet.sol): improve contract documentation

--- contracts/UsingWitnet.sol
+++ contracts/UsingWitnet.sol
@@ -3,39 +3,40 @@ pragma solidity ^0.5.0;
 import "./Request.sol";
 import "./Result.sol";
 import "./UsingWitnetBytes.sol";
+import "./WitnetBridgeInterface.sol";
 
 /**
- * @title The UsingWitnetRequest contract
+ * @title The UsingWitnetrequest contract
  * @notice Contract writers can inherit this contract in order to create requests for the
  * Witnet network
  */
-contract UsingWitnetRequest is UsingWitnetBytes {
+contract UsingWitnet is UsingWitnetBytes {
     /**
-    * @notice Include a new Data Request to be resolved by Witnet network
+    * @notice Send a new request to the Witnet network
     * @dev Call to `post_dr` function in the WitnetBridgeInterface contract
-    * @param _dr Data Request
-    * @param _tallyReward Reward specified for the user which post the Data Request result
-    * @return Identifier for the Data Request included in the WitnetBridgeInterface
+    * @param _dr An instance of the `Request` contract
+    * @param _tallyReward Reward specified for the user which post the request result
+    * @return Identifier for the request included in the WitnetBridgeInterface
     */
-    function witnetPostDataRequest(Request _dr, uint256 _tallyReward) public payable returns(uint256 id){
+    function witnetPostRequest(Request _dr, uint256 _tallyReward) public payable returns(uint256 id){
         return wbi.postDataRequest.value(msg.value)(_dr.serialized(), _tallyReward);
     }
 
     /**
-    * @notice Upgrade the rewards for a Data Request previously included
+    * @notice Upgrade the rewards for a request previously included
     * @dev Call to `upgrade_dr` function in the WitnetBridgeInterface contract
-    * @param _dr The Data Request included in the WitnetBridgeInterface
-    * @param _tallyReward Reward specified for the user which post the Data Request result
+    * @param _dr The request included in the WitnetBridgeInterface
+    * @param _tallyReward Reward specified for the user which post the request result
     */
-    function witnetUpgradeDataRequest(Request _dr, uint256 _tallyReward) public payable {
+    function witnetUpgradeRequest(Request _dr, uint256 _tallyReward) public payable {
         wbi.upgradeDataRequest.value(msg.value)(uint256(_dr.id()), _tallyReward);
     }
 
     /**
-    * @notice Read the result of a resolved Data Request
+    * @notice Read the result of a resolved request
     * @dev Call to `read_result` function in the WitnetBridgeInterface contract
-    * @param _dr The Data Request included in the WitnetBridgeInterface
-    * @return Data Request result
+    * @param _dr The request included in the WitnetBridgeInterface
+    * @return request result
     */
     function witnetReadResult (Request _dr) public returns(Result){
         return new Result(wbi.readResult(uint256(_dr.id())));
