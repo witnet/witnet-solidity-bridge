1f07d5fe181b4a540fd0bba238257bdcae377f28
Adán SDPC <adan.sdpc@gmail.com>
2019-09-11 19:37:00 +0200

    tests: fix Buffer tests

--- test/Buffer.sol
+++ test/Buffer.sol
@@ -5,14 +5,14 @@ import "../contracts/Buffer.sol";
 
 contract TestBuffer {
 
-    using BufferLib for BufferLib.buffer;
+    using BufferLib for BufferLib.Buffer;
 
     event Log(string _topic, uint256 _value);
 
     function testReadUint8() public {
         uint8 expected = 31;
         bytes memory data = abi.encodePacked(expected);
-        BufferLib.buffer memory buf = Buffer.buffer(data, 0);
+        BufferLib.Buffer memory buf = BufferLib.Buffer(data, 0);
         uint8 actual = buf.readUint8();
 
         Assert.equal(uint(actual), uint(expected), "Read Uint8 from a Buffer");
@@ -21,7 +21,7 @@ contract TestBuffer {
     function testReadUint16() public {
         uint16 expected = 31415;
         bytes memory data = abi.encodePacked(expected);
-        BufferLib.buffer memory buf = Buffer.buffer(data, 0);
+        BufferLib.Buffer memory buf = BufferLib.Buffer(data, 0);
 
         uint16 actual = buf.readUint16();
         Assert.equal(uint(actual), uint(expected), "Read Uint16 from a Buffer");
@@ -30,7 +30,7 @@ contract TestBuffer {
     function testReadUint32() public {
         uint32 expected = 3141592653;
         bytes memory data = abi.encodePacked(expected);
-        BufferLib.buffer memory buf = Buffer.buffer(data, 0);
+        BufferLib.Buffer memory buf = BufferLib.Buffer(data, 0);
 
         uint32 actual = buf.readUint32();
         Assert.equal(uint(actual), uint(expected), "Read Uint32 from a Buffer");
@@ -39,7 +39,7 @@ contract TestBuffer {
     function testReadUint64() public {
         uint64 expected = 3141592653589793238;
         bytes memory data = abi.encodePacked(expected);
-        BufferLib.buffer memory buf = Buffer.buffer(data, 0);
+        BufferLib.Buffer memory buf = BufferLib.Buffer(data, 0);
 
         uint64 actual = buf.readUint64();
         Assert.equal(uint(actual), uint(expected), "Read Uint64 from a Buffer");
@@ -49,7 +49,7 @@ contract TestBuffer {
         uint8 small = 31;
         uint64 big = 3141592653589793238;
         bytes memory data = abi.encodePacked(small, big);
-        BufferLib.buffer memory buf = Buffer.buffer(data, 0);
+        BufferLib.Buffer memory buf = BufferLib.Buffer(data, 0);
 
         buf.readUint8();
         uint64 actualBig = buf.readUint64();
