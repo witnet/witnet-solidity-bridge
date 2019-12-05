7edd324628731d61589383d9088467df0bf053bb
Adán SDPC <adan.sdpc@gmail.com>
2019-12-05 10:59:43 +0100

    feat: support decoding of RADON error codes and messages

--- contracts/Witnet.sol
+++ contracts/Witnet.sol
@@ -20,8 +20,39 @@ library Witnet {
     ENUMS
   */
   enum ErrorCodes {
-    RuntimeError,               // The tally script failed during runtime.
-    InsufficientConsensusError  // The tally did not fulfill the consensus requirement of the request.
+    // Unknown error. Something went really bad!
+    Unknown,
+    // Script format errors
+    /// At least one of the source scripts is not a valid CBOR-encoded value.
+    SourceScriptNotCBOR,
+    /// The CBOR value decoded from a source script is not an Array.
+    SourceScriptNotArray,
+    /// The Array value decoded form a source script is not a valid RADON script.
+    SourceScriptNotRADON,
+    // Complexity errors
+    /// The request contains too many sources.
+    RequestTooManySources,
+    /// The script contains too many calls.
+    ScriptTooManyCalls,
+    // Operator errors
+    /// The operator does not exist.
+    UnsupportedOperator,
+    // Retrieval-specific errors
+    /// At least one of the sources could not be retrieved, but returned HTTP error.
+    HTTP,
+    // Math errors
+    /// Math operator caused an underflow.
+    Underflow,
+    /// Math operator caused an overflow.
+    Overflow,
+    /// Tried to divide by zero.
+    DivisionByZero,
+    // The tally script failed during runtime.
+    RuntimeError,
+    // The tally did not fulfill the consensus requirement of the request.
+    InsufficientConsensusError,
+    // This is not a real error but a way to mark the size of the `enum`
+    Size
   }
 
   /*
@@ -70,13 +101,76 @@ library Witnet {
 
   /**
    * @notice Decode an error code from a Result as a member of `ErrorCodes`
-   * @param _result An instance of Result
+   * @param _result An instance of `Result`
    * @return The `CBORValue.Error memory` decoded from the Result
    */
-  function asError(Result memory _result) public pure returns(ErrorCodes) {
+  function asErrorCode(Result memory _result) public pure returns(ErrorCodes) {
+    uint64[] memory error = asRawError(_result);
+
+    // Decode the error code only if it belongs to the `ErrorCodes` enum — otherwise, default to `ErrorCodes.Unknown`
+    if (error[0] <= uint8(ErrorCodes.Size)) {
+      return ErrorCodes(error[0]);
+    } else {
+      return ErrorCodes.Unknown;
+    }
+  }
+
+  /**
+   * @notice Generate a suitable error message for a member of `ErrorCodes` and its corresponding arguments
+   * @param _result An instance of `Result`
+   * @return A tuple containing the `CBORValue.Error memory` decoded from the `Result`, plus a loggable error message.
+   */
+  function asErrorMessage(Result memory _result) public pure returns(ErrorCodes, string memory) {
+    uint64[] memory error = asRawError(_result);
+    ErrorCodes errorCode;
+    bytes memory errorMessage;
+
+    // Decode the error code only if it belongs to the `ErrorCodes` enum — otherwise, default to `ErrorCodes.Unknown`
+    if (error[0] <= uint8(ErrorCodes.Size)) {
+      errorCode = ErrorCodes(error[0]);
+    } else {
+      errorCode = ErrorCodes.Unknown;
+    }
+
+    if (errorCode == ErrorCodes.SourceScriptNotCBOR) {
+      errorMessage = abi.encodePacked("Source script #", utoa(error[1]), " was not a valid CBOR value");
+    } else if (errorCode == ErrorCodes.SourceScriptNotArray) {
+      errorMessage = abi.encodePacked("The CBOR value in script #", utoa(error[1]), " was not an Array of calls");
+    } else if (errorCode == ErrorCodes.SourceScriptNotRADON) {
+      errorMessage = abi.encodePacked("The CBOR value in script #", utoa(error[1]), " was not a valid RADON script");
+    } else if (errorCode == ErrorCodes.RequestTooManySources) {
+      errorMessage = abi.encodePacked("The request contained too many sources (", utoa(error[1]), ")");
+    } else if (errorCode == ErrorCodes.ScriptTooManyCalls) {
+      errorMessage = abi.encodePacked("Script #", utoa(error[2]), " from the ", stageName(error[1]), " stage contained too many calls (", utoa(error[3]), ")");
+    } else if (errorCode == ErrorCodes.UnsupportedOperator) {
+      errorMessage = abi.encodePacked("Operator code ", utoa(error[4]), " found at call #", utoa(error[3]), " in script #", utoa(error[2]), " from ", stageName(error[1]), " stage is not supported");
+    } else if (errorCode == ErrorCodes.HTTP) {
+      errorMessage = abi.encodePacked("Source #", utoa(error[1]), " could not be retrieved. Failed with HTTP error code: ", utoa(error[2] / 100), utoa(error[2] % 100 / 10), utoa(error[2] % 10));
+    } else if (errorCode == ErrorCodes.Underflow) {
+      errorMessage = abi.encodePacked("Underflow at operator code ", utoa(error[4]), " found at call #", utoa(error[3]), " in script #", utoa(error[2]), " from ", stageName(error[1]), " stage");
+    } else if (errorCode == ErrorCodes.Overflow) {
+      errorMessage = abi.encodePacked("Overflow at operator code ", utoa(error[4]), " found at call #", utoa(error[3]), " in script #", utoa(error[2]), " from ", stageName(error[1]), " stage");
+    } else if (errorCode == ErrorCodes.DivisionByZero) {
+      errorMessage = abi.encodePacked("Division by zero at operator code ", utoa(error[4]), " found at call #", utoa(error[3]), " in script #", utoa(error[2]), " from ", stageName(error[1]), " stage");
+    } else if (errorCode == ErrorCodes.RuntimeError) {
+      errorMessage = abi.encodePacked("Unspecified runtime error at operator code ", utoa(error[4]), " found at call #", utoa(error[3]), " in script #", utoa(error[2]), " from ", stageName(error[1]), " stage");
+    } else if (errorCode == ErrorCodes.InsufficientConsensusError) {
+      errorMessage = abi.encodePacked("Insufficient consensus. Required: ", utoa(error[1]), ". Achieved: ", utoa(error[2]));
+    } else {
+      errorMessage = abi.encodePacked("Unknown error (", utoa(error[0]), ")");
+    }
+
+    return (errorCode, string(errorMessage));
+  }
+
+  /**
+   * @notice Decode a raw error from a `Result` as a `uint64[]`
+   * @param _result An instance of `Result`
+   * @return The `uint64[]` raw error as decoded from the `Result`
+   */
+  function asRawError(Result memory _result) public pure returns(uint64[] memory) {
     require(!_result.success, "Tried to read error code from successful Result");
-    uint64 errorCode = _result.cborValue.decodeUint64();
-    return ErrorCodes(errorCode);
+    return _result.cborValue.decodeUint64Array();
   }
 
   /**
@@ -133,10 +227,10 @@ library Witnet {
   }
 
   /**
-  * @notice Decode an array of string values from a Result as a `string[]` value
-  * @param _result An instance of Result
-  * @return The `string[]` decoded from the Result
-  */
+   * @notice Decode an array of string values from a Result as a `string[]` value
+   * @param _result An instance of Result
+   * @return The `string[]` decoded from the Result
+   */
   function asStringArray(Result memory _result) public pure returns(string[] memory) {
     require(_result.success, "Tried to read `string[]` value from errored Result");
     return _result.cborValue.decodeStringArray();
@@ -153,13 +247,53 @@ library Witnet {
   }
 
   /**
- * @notice Decode an array of natural numeric values from a Result as a `uint64[]` value
- * @param _result An instance of Result
- * @return The `uint64[]` decoded from the Result
- */
+   * @notice Decode an array of natural numeric values from a Result as a `uint64[]` value
+   * @param _result An instance of Result
+   * @return The `uint64[]` decoded from the Result
+   */
   function asUint64Array(Result memory _result) public pure returns(uint64[] memory) {
     require(_result.success, "Tried to read `uint64[]` value from errored Result");
     return _result.cborValue.decodeUint64Array();
   }
 
+  /**
+   * @notice Convert a stage index number into the name of the matching Witnet request stage
+   * @param _stageIndex A `uint64` identifying the index of one of the Witnet request stages
+   * @return The name of the matching stage
+   */
+  function stageName(uint64 _stageIndex) public pure returns(string memory) {
+    if (_stageIndex == 0) {
+      return "retrieval";
+    } else if (_stageIndex == 1) {
+      return "aggregation";
+    } else if (_stageIndex == 2) {
+      return "tally";
+    } else {
+      return "unknown";
+    }
+  }
+
+  /**
+   * @notice Convert a `uint8` into a 1, 2 or 3 characters long `string` representing its decimal value
+   * @param _u A `uint8` value
+   * @return The `string` representing its decimal value
+   */
+  function utoa(uint64 _u) private pure returns(string memory) {
+    if (_u < 10) {
+      bytes memory b1 = new bytes(1);
+      b1[0] = byte(uint8(_u) + 48);
+      return string(b1);
+    } else if (_u < 100) {
+      bytes memory b2 = new bytes(2);
+      b2[0] = byte(uint8(_u / 10) + 48);
+      b2[1] = byte(uint8(_u % 10) + 48);
+      return string(b2);
+    } else {
+      bytes memory b3 = new bytes(3);
+      b3[0] = byte(uint8(_u / 100) + 48);
+      b3[1] = byte(uint8(_u % 100 / 10) + 48);
+      b3[2] = byte(uint8(_u % 10) + 48);
+      return string(b3);
+    }
+  }
 }
---
+++ test/Witnet.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+import "truffle/Assert.sol";
+import "../contracts/Witnet.sol";
+
+contract WitnetTest {
+  using Witnet for Witnet.Result;
+
+  // Test the `Witnet.stageNames` pure method, which gives strings with the names for the different Witnet request
+  // stages
+  function testStageNames() public {
+    Assert.equal(Witnet.stageName(0), "retrieval", "Stage name for stage #1 should be \"retrieval\"");
+    Assert.equal(Witnet.stageName(1), "aggregation", "Stage name for stage #1 should be \"aggregation\"");
+    Assert.equal(Witnet.stageName(2), "tally", "Stage name for stage #1 should be \"tally\"");
+  }
+
+  // Test decoding of `RadonError` error codes
+  function testErrorCodes() public {
+    Witnet.ErrorCodes errorCode0 = Witnet.resultFromCborBytes(hex"D8278100").asErrorCode();
+    Witnet.ErrorCodes errorCode1 = Witnet.resultFromCborBytes(hex"D8278101").asErrorCode();
+    Witnet.ErrorCodes errorCode2 = Witnet.resultFromCborBytes(hex"D8278102").asErrorCode();
+    Witnet.ErrorCodes errorCode3 = Witnet.resultFromCborBytes(hex"D8278103").asErrorCode();
+    Witnet.ErrorCodes errorCode4 = Witnet.resultFromCborBytes(hex"D8278104").asErrorCode();
+    Witnet.ErrorCodes errorCode5 = Witnet.resultFromCborBytes(hex"D8278105").asErrorCode();
+    Witnet.ErrorCodes errorCode6 = Witnet.resultFromCborBytes(hex"D8278106").asErrorCode();
+    Witnet.ErrorCodes errorCode7 = Witnet.resultFromCborBytes(hex"D8278107").asErrorCode();
+    Witnet.ErrorCodes errorCode8 = Witnet.resultFromCborBytes(hex"D8278108").asErrorCode();
+    Witnet.ErrorCodes errorCode9 = Witnet.resultFromCborBytes(hex"D8278109").asErrorCode();
+    Witnet.ErrorCodes errorCode10 = Witnet.resultFromCborBytes(hex"D827810A").asErrorCode();
+    Witnet.ErrorCodes errorCode11 = Witnet.resultFromCborBytes(hex"D827810B").asErrorCode();
+    Witnet.ErrorCodes errorCode12 = Witnet.resultFromCborBytes(hex"D827810C").asErrorCode();
+    Assert.equal(uint(errorCode0), uint(Witnet.ErrorCodes.Unknown), "Error code #0 should be `Witnet.ErrorCodes.Unknown`");
+    Assert.equal(uint(errorCode1), uint(Witnet.ErrorCodes.SourceScriptNotCBOR), "Error code #1 should be `Witnet.ErrorCodes.SourceScriptNotCBOR`");
+    Assert.equal(uint(errorCode2), uint(Witnet.ErrorCodes.SourceScriptNotArray), "Error code #2 should be `Witnet.ErrorCodes.SourceScriptNotArray`");
+    Assert.equal(uint(errorCode3), uint(Witnet.ErrorCodes.SourceScriptNotRADON), "Error code #3 should be `Witnet.ErrorCodes.SourceScriptNotRADON`");
+    Assert.equal(uint(errorCode4), uint(Witnet.ErrorCodes.RequestTooManySources), "Error code #4 should be `Witnet.ErrorCodes.RequestTooManySources`");
+    Assert.equal(uint(errorCode5), uint(Witnet.ErrorCodes.ScriptTooManyCalls), "Error code #5 should be `Witnet.ErrorCodes.ScriptTooManyCalls`");
+    Assert.equal(uint(errorCode6), uint(Witnet.ErrorCodes.UnsupportedOperator), "Error code #6 should be `Witnet.ErrorCodes.UnsupportedOperator`");
+    Assert.equal(uint(errorCode7), uint(Witnet.ErrorCodes.HTTP), "Error code #7 should be `Witnet.ErrorCodes.HTTP`");
+    Assert.equal(uint(errorCode8), uint(Witnet.ErrorCodes.Underflow), "Error code #8 should be `Witnet.ErrorCodes.Underflow`");
+    Assert.equal(uint(errorCode9), uint(Witnet.ErrorCodes.Overflow), "Error code #9 should be `Witnet.ErrorCodes.Overflow`");
+    Assert.equal(uint(errorCode10), uint(Witnet.ErrorCodes.DivisionByZero), "Error code #10 should be `Witnet.ErrorCodes.DivisionByZero`");
+    Assert.equal(uint(errorCode11), uint(Witnet.ErrorCodes.RuntimeError), "Error code #11 should be `Witnet.ErrorCodes.RuntimeError`");
+    Assert.equal(uint(errorCode12), uint(Witnet.ErrorCodes.InsufficientConsensusError), "Error code #12 should be `Witnet.ErrorCodes.InsufficientConsensusError`");
+  }
+
+  // Test decoding of `RadonError` error messages
+  function testErrorMessages() public {
+    (, string memory errorMessage0) = Witnet.resultFromCborBytes(hex"D8278100").asErrorMessage();
+    (, string memory errorMessage1) = Witnet.resultFromCborBytes(hex"D827820102").asErrorMessage();
+    (, string memory errorMessage2) = Witnet.resultFromCborBytes(hex"D827820203").asErrorMessage();
+    (, string memory errorMessage3) = Witnet.resultFromCborBytes(hex"D827820304").asErrorMessage();
+    (, string memory errorMessage4) = Witnet.resultFromCborBytes(hex"D827820405").asErrorMessage();
+    (, string memory errorMessage5) = Witnet.resultFromCborBytes(hex"D8278405000708").asErrorMessage();
+    (, string memory errorMessage6) = Witnet.resultFromCborBytes(hex"D82785060108090A").asErrorMessage();
+    (, string memory errorMessage7) = Witnet.resultFromCborBytes(hex"D827830708190141").asErrorMessage();
+    (, string memory errorMessage8) = Witnet.resultFromCborBytes(hex"D827850802090A0B").asErrorMessage();
+    (, string memory errorMessage9) = Witnet.resultFromCborBytes(hex"D8278509000A0B0C").asErrorMessage();
+    (, string memory errorMessage10) = Witnet.resultFromCborBytes(hex"D827850A010B0C0D").asErrorMessage();
+    (, string memory errorMessage11) = Witnet.resultFromCborBytes(hex"D827850B020C0D0E").asErrorMessage();
+    (, string memory errorMessage12) = Witnet.resultFromCborBytes(hex"D827830C0D0E").asErrorMessage();
+    (, string memory errorMessage255) = Witnet.resultFromCborBytes(hex"D8278118FF").asErrorMessage();
+    Assert.equal(errorMessage0, "Unknown error (0)", "Error message #0 should be properly formatted");
+    Assert.equal(errorMessage1, "Source script #2 was not a valid CBOR value", "Error message #1 (`Witnet.ErrorCodes.SourceScriptNotCBOR`) should be properly formatted");
+    Assert.equal(errorMessage2, "The CBOR value in script #3 was not an Array of calls", "Error message #2 (`Witnet.ErrorCodes.SourceScriptNotArray`) should be properly formatted");
+    Assert.equal(errorMessage3, "The CBOR value in script #4 was not a valid RADON script", "Error message #3 (`Witnet.ErrorCodes.SourceScriptNotRADON`) should be properly formatted");
+    Assert.equal(errorMessage4, "The request contained too many sources (5)", "Error message #4 (`Witnet.ErrorCodes.RequestTooManySources`) should be properly formatted");
+    Assert.equal(errorMessage5, "Script #7 from the retrieval stage contained too many calls (8)", "Error message #5 (`Witnet.ErrorCodes.ScriptTooManyCalls`) should be properly formatted");
+    Assert.equal(errorMessage6, "Operator code 10 found at call #9 in script #8 from aggregation stage is not supported", "Error message #6 (`Witnet.ErrorCodes.UnsupportedOperator`) should be properly formatted");
+    Assert.equal(errorMessage7, "Source #8 could not be retrieved. Failed with HTTP error code: 321", "Error message #7 (`Witnet.ErrorCodes.HTTP`) should be properly formatted");
+    Assert.equal(errorMessage8, "Underflow at operator code 11 found at call #10 in script #9 from tally stage", "Error message #8 (`Witnet.ErrorCodes.Underflow`) should be properly formatted");
+    Assert.equal(errorMessage9, "Overflow at operator code 12 found at call #11 in script #10 from retrieval stage", "Error message #9 (`Witnet.ErrorCodes.Overflow`) should be properly formatted");
+    Assert.equal(errorMessage10, "Division by zero at operator code 13 found at call #12 in script #11 from aggregation stage", "Error message #10 (`Witnet.ErrorCodes.DivisionByZero`) should be properly formatted");
+    Assert.equal(errorMessage11, "Unspecified runtime error at operator code 14 found at call #13 in script #12 from tally stage", "Error message #11 (`Witnet.ErrorCodes.RuntimeError`) should be properly formatted");
+    Assert.equal(errorMessage12, "Insufficient consensus. Required: 13. Achieved: 14", "Error message #12 (`Witnet.ErrorCodes.InsufficientConsensusError`) should be properly formatted");
+    Assert.equal(errorMessage255, "Unknown error (255)", "Error message for an unknown error should be properly formatted");
+  }
+}
