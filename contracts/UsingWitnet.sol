a6ded275ada35878eb6edc30c8ebf67a2f7560bd
Mario Cao <mariocao@gmail.com>
2020-03-27 18:17:04 +0100

    chore: update migrations addresses

--- contracts/UsingWitnet.sol
+++ contracts/UsingWitnet.sol
@@ -31,14 +31,15 @@ contract UsingWitnet {
   }
 
   /**
-  * @notice Send a new request to the Witnet network.
-  * @dev Call to `post_dr` function in the WitnetRequestsBoard contract.
-  * @param _request An instance of the `Request` contract.
-  * @param _tallyReward Reward specified for the user which post the Data Request result.
-  * @return Sequencial identifier for the request included in the WitnetRequestsBoard.
+  * @notice Send a new request to the Witnet network
+  * @dev Call to `post_dr` function in the WitnetRequestsBoard contract
+  * @param _request An instance of the `Request` contract
+  * @param _requestReward Reward specified for the user which posts the request into Witnet
+  * @param _resultReward Reward specified for the user which posts back the request result
+  * @return Sequencial identifier for the request included in the WitnetRequestsBoard
   */
-  function witnetPostRequest(Request _request, uint256 _tallyReward) internal returns (uint256) {
-    return wrb.postDataRequest.value(msg.value)(_request.bytecode(), _tallyReward);
+  function witnetPostRequest(Request _request, uint256 _requestReward, uint256 _resultReward) internal returns (uint256) {
+    return wrb.postDataRequest.value(_requestReward + _resultReward)(_request.bytecode(), _resultReward);
   }
 
   /**
--- test/helpers/UsingWitnetTestHelper.sol
+++ test/helpers/UsingWitnetTestHelper.sol
@@ -19,8 +19,8 @@ contract UsingWitnetTestHelper is UsingWitnet {
 
   constructor (address _wrbAddress) UsingWitnet(_wrbAddress) public { }
 
-  function _witnetPostRequest(Request _request, uint256 _tallyReward) external payable returns(uint256 id) {
-    return witnetPostRequest(_request, _tallyReward);
+  function _witnetPostRequest(Request _request, uint256 _requestReward, uint256 _tallyReward) external payable returns(uint256 id) {
+    return witnetPostRequest(_request, _requestReward, _tallyReward);
   }
 
   function _witnetUpgradeRequest(uint256 _id, uint256 _tallyReward) external payable {
