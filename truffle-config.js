23b2c730d2bb2a93f7920bc411b9a1ff1f512ccf
Mario Cao <mariocao@gmail.com>
2020-12-04 14:02:38 +0100

    docs: add gas report

--- truffle-config.js
+++ truffle-config.js
@@ -28,7 +28,7 @@ module.exports = {
     reporter: 'eth-gas-reporter',
     reporterOptions: {
         currency: "USD",
-        gasPrice: 100,
+        gasPrice: 50,
         excludeContracts: ['Migrations'],
         src: "contracts"
     },
