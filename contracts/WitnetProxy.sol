d6a6ce149693910a29239e1378113d63f31f3c17
Guillermo Díaz <guillermo@otherplane.com>
2021-06-23 13:13:21 +0200

    refactor(proxy): delegatecalls and no biz logic

---
+++ contracts/WitnetProxy.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.5;
+
+import "./utils/Upgradable.sol";
+
+contract WitnetProxy {
+  Upgradable public wrb;
+
+  constructor () {}
+
+  receive() external payable {
+    revert("WitnetProxy: no ETH accepted");
+  }
+
+  fallback() external payable {
+    address _wrb = address(wrb);
+    assembly {
+      let ptr := mload(0x40)
+      calldatacopy(ptr, 0, calldatasize())
+      let result := delegatecall(gas(), _wrb, ptr, calldatasize(), 0, 0)
+      let size := returndatasize()
+      returndatacopy(ptr, 0, size)
+      switch result
+        case 0  { revert(ptr, size) }
+        default { return(ptr, size) }
+    }
+  }
+
+  function upgrade(address _newWrb, bytes memory _initData) public {
+    require(_newWrb != address(0), "WitnetProxy: null wrb");
+    require(_newWrb != address(wrb), "WitnetProxy: nothing to upgrade");
+    if (address(wrb) != address(0)) {
+      require(wrb.isUpgradable(), "WitnetProxy: not upgradable");
+    }
+    (bool _wasInitialized,) = _newWrb.delegatecall(
+      abi.encodeWithSignature("initialize(bytes)", _initData)
+    );
+    require(_wasInitialized, "WitnetProxy: unable to initialize new wrb");
+    wrb = Upgradable(_newWrb);
+  }
+}
