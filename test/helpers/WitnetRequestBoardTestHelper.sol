6bbf6b8e98a19cb21657ca71f5b8f7d66b399db3
Guillermo Díaz <guillermo@otherplane.com>
2021-06-23 13:50:48 +0200

    refactor(wrb): delegatable implementation

--- test/helpers/WitnetRequestBoardTestHelper.sol
+++ test/helpers/WitnetRequestBoardTestHelper.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.4;
+pragma solidity 0.8.5;
 
 // import "../../contracts/data/WitnetBoardData.sol";
-import "../../contracts/exports/IWitnetRequestor.sol";
-
+import "../../contracts/impls/centralized/WitnetRequestBoard.sol";
 
 /**
  * @title Witnet Requests Board Version 1
@@ -14,23 +13,9 @@ import "../../contracts/exports/IWitnetRequestor.sol";
   * The contract has been created for testing purposes
  * @author Witnet Foundation
  */
-contract WitnetRequestBoardTestHelper is IWitnetRequestor {
-
-  struct DataRequest {
-    address requestAddress;
-    uint256 reward;
-    bytes result;
-    uint256 timestamp;
-    uint256 drTxHash;
-    address payable pkhClaim;
-  }
-
-  DataRequest[] public requests;
+contract WitnetRequestBoardTestHelper is WitnetRequestBoard {
 
   address public witnet;
-
-  // List of addresses authorized to post blocks
-  address[] public committee;
 
   // Is upgradable
   bool public upgradable;
@@ -40,66 +25,43 @@ contract WitnetRequestBoardTestHelper is IWitnetRequestor {
     upgradable = _upgradable;
 
     // Insert an empty request so as to initialize the requests array with length > 0
-    DataRequest memory request;
-    requests.push(request);
-    committee = _committee;
+    setReporters(_committee);
   }
 
   /// @dev Posts a data request into the WRB in expectation that it will be relayed and resolved in Witnet with a total reward that equals to msg.value.
   /// @param _requestAddress The request contract address which includes the request bytecode.
-  /// @return The unique identifier of the data request.
-  function postDataRequest(address _requestAddress) external payable override returns(uint256) {
-    uint256 _id = requests.length;
-    DataRequest memory dr;
-    requests.push(dr);
-
-    requests[_id].requestAddress = _requestAddress;
-    requests[_id].reward = msg.value;
-    requests[_id].result = "hello";
-    requests[_id].timestamp = 0;
-    requests[_id].drTxHash = 0;
-    requests[_id].pkhClaim = payable(address(0));
-
-    emit PostedRequest(_id);
-
-    return _id;
-  }
-
-  /// @dev Increments the rewards of a data request by adding more value to it.
-  /// @param _id The unique identifier of the data request.
-  function upgradeDataRequest(uint256 _id)
-    external
-    payable
-    override
+  /// @return _id The unique identifier of the data request.
+  function postDataRequest(address _requestAddress)
+    public payable override returns(uint256 _id)
   {
-    requests[_id].reward += msg.value;
+    _id = super.postDataRequest(_requestAddress);
+    __data().requests[_id].result = "hello";
   }
 
   /// @dev Retrieves hash of the data request transaction in Witnet
   /// @param _id The unique identifier of the data request.
   /// @return The hash of the DataRequest transaction in Witnet
-  function readDrTxHash (uint256 _id) external view returns(uint256) {
-    return requests[_id].drTxHash;
+  function readDrTxHash(uint256 _id) external view override returns(uint256) {
+    return __dataRequest(_id).txhash;
   }
 
   /// @dev Retrieves the result (if already available) of one data request from the WRB.
   /// @param _id The unique identifier of the data request.
   /// @return The result of the DR
-  function readResult (uint256 _id) external view returns(bytes memory) {
-    return requests[_id].result;
+  function readResult(uint256 _id) external view override returns (WitnetTypes.Result memory) {
+    return Witnet.resultFromCborBytes(__data().requests[_id].result);
   }
 
   /// @dev Verifies if the contract is upgradable
   /// @return true if the contract upgradable
   /* solhint-disable-next-line no-unused-vars*/
-  function isUpgradable(address) external view returns(bool) {
+  function isUpgradable() external view override returns (bool) {
     return upgradable;
   }
 
   /// @dev Estimate the amount of reward we need to insert for a given gas price.
   /// @return The rewards to be included for the given gas price as inclusionReward, resultReward, blockReward.
-  function estimateGasCost(uint256) external view override returns(uint256){
+  function estimateGasCost(uint256) external pure override returns(uint256){
     return 0;
   }
-
 }
