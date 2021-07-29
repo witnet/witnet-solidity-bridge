4a6835c830341a1fbef8a61db27560aa9de9d059
Guillermo Díaz <guillermo@otherplane.com>
2021-07-29 16:38:10 +0200

    refactor(proxy): `delegate` to `implementation`

--- contracts/WitnetProxy.sol
+++ contracts/WitnetProxy.sol
@@ -12,7 +12,9 @@ import "./utils/Upgradable.sol";
  * Written in 2021 by the Witnet Foundation.
  **/
 contract WitnetProxy {
-  Upgradable public delegate;
+  struct WitnetProxyData {
+    address implementation;
+  }
 
   /// @dev Constructor with no params as to ease eventual support of Singleton pattern (i.e. ERC-2470)
   constructor () {}
@@ -24,15 +26,15 @@ contract WitnetProxy {
 
   /// @dev Payable fallback accepts delegating calls to payable functions.  
   fallback() external payable { /* solhint-disable no-complex-fallback */
-    address _delegate = address(delegate);
+    address _implementation = implementation();
 
     assembly { /* solhint-disable avoid-low-level-calls */
-      // Gas optimized delegate call to 'delegate' contract.
+      // Gas optimized delegate call to 'implementation' contract.
       // Note: `msg.data`, `msg.sender` and `msg.value` will be passed over 
-      //       to actual implementation of `msg.sig` within `delegate` contract.
+      //       to actual implementation of `msg.sig` within `implementation` contract.
       let ptr := mload(0x40)
       calldatacopy(ptr, 0, calldatasize())
-      let result := delegatecall(gas(), _delegate, ptr, calldatasize(), 0, 0)
+      let result := delegatecall(gas(), _implementation, ptr, calldatasize(), 0, 0)
       let size := returndatasize()
       returndatacopy(ptr, 0, size)
       switch result
@@ -41,35 +43,41 @@ contract WitnetProxy {
           revert(ptr, size) 
         }
         default {
-          // pass back same data as returned by 'delegate' contract:
+          // pass back same data as returned by 'implementation' contract:
           return(ptr, size) 
         }
     }
   }
 
-  /// @dev Upgrades the `delegate` address.
-  /// @param _newDelegate New implementation address.
+  /// @dev Returns proxy's current implementation address.
+  function implementation() public view returns (address) {
+    return __proxySlot().implementation;
+  }
+
+  /// @dev Upgrades the `implementation` address.
+  /// @param _newImplementation New implementation address.
   /// @param _initData Raw data with which new implementation will be initialized.
   /// @return Returns whether new implementation would be further upgradable, or not.
-  function upgrade(address _newDelegate, bytes memory _initData)
+  function upgrade(address _newImplementation, bytes memory _initData)
     public returns (bool)
   {
-    // New delegate cannot be null:
-    require(_newDelegate != address(0), "WitnetProxy: null delegate");
+    // New implementation cannot be null:
+    require(_newImplementation != address(0), "WitnetProxy: null implementation");
 
-    if (address(delegate) != address(0)) {
-      // New delegate address must differ from current one:
-      require(_newDelegate != address(delegate), "WitnetProxy: nothing to upgrade");
+    address _oldImplementation = implementation();
+    if (_oldImplementation != address(0)) {
+      // New implementation address must differ from current one:
+      require(_newImplementation != _oldImplementation, "WitnetProxy: nothing to upgrade");
 
       // Assert whether current implementation is intrinsically upgradable:
-      try delegate.isUpgradable() returns (bool _isUpgradable) {
+      try Upgradable(_oldImplementation).isUpgradable() returns (bool _isUpgradable) {
         require(_isUpgradable, "WitnetProxy: not upgradable");
       } catch {
         revert("WitnetProxy: unable to check upgradability");
       }
 
       // Assert whether current implementation allows `msg.sender` to upgrade the proxy:
-      (bool _wasCalled, bytes memory _result) = address(delegate).delegatecall(
+      (bool _wasCalled, bytes memory _result) = _oldImplementation.delegatecall(
         abi.encodeWithSignature(
           "isUpgradableFrom(address)",
           msg.sender
@@ -80,7 +88,7 @@ contract WitnetProxy {
     }
 
     // Initialize new implementation within proxy-context storage:
-    (bool _wasInitialized,) = _newDelegate.delegatecall(
+    (bool _wasInitialized,) = _newImplementation.delegatecall(
       abi.encodeWithSignature(
         "initialize(bytes)",
         _initData
@@ -89,10 +97,10 @@ contract WitnetProxy {
     require(_wasInitialized, "WitnetProxy: unable to initialize");
 
     // If all checks and initialization pass, update implementation address:
-    delegate = Upgradable(_newDelegate);
+    __proxySlot().implementation = _newImplementation;
 
-    // Asserts new delegate complies w/ minimal implementation of Upgradable interface:
-    try delegate.isUpgradable() returns (bool _isUpgradable) {
+    // Asserts new implementation complies w/ minimal implementation of Upgradable interface:
+    try Upgradable(_newImplementation).isUpgradable() returns (bool _isUpgradable) {
       return _isUpgradable;
     }
     catch {
