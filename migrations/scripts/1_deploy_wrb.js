guidiaz/witnet-ethereum-bridge

feat: decouple using witnet

migrations/scripts/1_deploy_wrb.js
@@ -1,0 +1,139 @@
+const { merge } = require("lodash")
+
+const realm = process.env.WITNET_EVM_REALM ? process.env.WITNET_EVM_REALM.toLowerCase() : "default"
+const settings = require("../settings.witnet")
+const artifactsName = merge(settings.artifacts.default, settings.artifacts[realm])
+
+module.exports = async function (deployer, network, accounts) {
+  const addresses = require("../addresses.witnet")[realm][network.split("-")[0]]
+
+  let WitnetParserLib, WitnetDecoderLib
+  let WitnetProxy, WitnetRequestBoard
+  let upgradeProxy = false
+
+  /* Load the 'WitnetProxy' artifact only if neccesary, and deploy it if not done yet */
+  try {
+    WitnetProxy = artifacts.require(artifactsName.WitnetProxy)
+    if (!WitnetProxy.isDeployed() || WitnetProxy.address !== addresses.WitnetRequestBoard) {
+      // If the 'WitnetProxy' artifact is found, we assume that the
+      // target 'WitnetRequestBoard' implementation is proxiable. In this case,
+      // the 'WitnetProxy' actual address will be read from the one assigned to the
+      // 'WitnetRequestBoard' artifact in the addresses file:
+      if (addresses) WitnetProxy.address = addresses.WitnetRequestBoard
+    }
+    if (!WitnetProxy.isDeployed() || isNullAddress(WitnetProxy.address)) {
+      await deployer.deploy(WitnetProxy)
+    } else {
+      console.log(`\n   Skipped: '${artifactsName.WitnetProxy}' deployed at ${WitnetProxy.address}.`)
+    }
+    upgradeProxy = true
+  } catch {
+    // If no 'WitnetProxy' artifact is found, we assume that
+    // the target 'WitnetRequestBoard' implementation is not proxiable.
+  }
+
+  /* Load target 'WitnetRequestBoard' implementation artifact */
+  try {
+    WitnetRequestBoard = artifacts.require(artifactsName.WitnetRequestBoard)
+  } catch {
+    console.log(`\n   Skipped: '${artifactsName.WitnetRequestBoard}' artifact not found.`)
+    return
+  }
+
+  if (!upgradeProxy) {
+    if (!WitnetRequestBoard.isDeployed() || isNullAddress(WitnetRequestBoard.address)) {
+      // Read implementation address from file only if the implementation requires no proxy
+      if (addresses) WitnetRequestBoard.address = addresses.WitnetRequestBoard
+    }
+  }
+
+  /* Try to find 'WitnetParserLib' artifact, and deployed address if any */
+  try {
+    WitnetParserLib = artifacts.require(artifactsName.WitnetParserLib)
+    if (!WitnetParserLib.isDeployed() || WitnetParserLib.address !== addresses.WitnetParserLib) {
+      // If the 'WitnetParserLib' is found, try to read deployed address from addresses file:
+      if (addresses) WitnetParserLib.address = addresses.WitnetParserLib
+    }
+  } catch {
+    console.error(`\n   Fatal: '${artifactsName.WitnetParserLib}' artifact not found.\n`)
+    process.exit(1)
+  }
+
+  /* Deploy new instance of 'WitnetParserLib', and 'WitnetDecoderLib', if neccesary */
+  if (!WitnetParserLib.isDeployed() || isNullAddress(WitnetParserLib.address)) {
+    // Fetch the 'WitnetDecoderLib' artifact:
+    try {
+      WitnetDecoderLib = artifacts.require(artifactsName.WitnetDecoderLib)
+    } catch {
+      console.error(`\n   Fatal: '${artifactsName.WitnetDecoderLib}' artifact not found.\n`)
+      process.exit(1)
+    }
+    // Deploy the 'WitnetDecoderLib' artifact first, if not done yet:
+    if (!WitnetDecoderLib.isDeployed()) {
+      await deployer.deploy(WitnetDecoderLib)
+      // and link the just-deployed 'WitnetDecoderLib' to the 'WitnetParserLib' artifact:
+      await deployer.link(WitnetDecoderLib, WitnetParserLib)
+    }
+    await deployer.deploy(WitnetParserLib)
+  } else {
+    console.log(`\n   Skipped: '${artifactsName.WitnetParserLib}' deployed at ${WitnetParserLib.address}.`)
+  }
+
+  /* Deploy new instance of target 'WitnetRequestBoard' implementation */
+  await deployer.link(WitnetParserLib, WitnetRequestBoard)
+  await deployer.deploy(
+    WitnetRequestBoard,
+    ...(
+    // use realm-specific constructor parameters, if defined...
+      settings.constructorParams[realm] && settings.constructorParams[realm].WitnetRequestBoard
+        ? settings.constructorParams[realm].WitnetRequestBoard
+        : settings.constructorParams.default.WitnetRequestBoard
+    )
+  )
+
+  /* Upgrade 'WitnetProxy' instance, if neccesary */
+  if (upgradeProxy) {
+    const proxy = await WitnetProxy.deployed()
+    const wrb = await WitnetRequestBoard.deployed()
+
+    const oldAddr = await proxy.implementation.call()
+    let oldCodehash, oldVersion
+    if (!isNullAddress(oldAddr)) {
+      const oldWrb = await WitnetRequestBoard.at(oldAddr)
+      oldCodehash = await oldWrb.codehash.call()
+      oldVersion = await oldWrb.version.call()
+    }
+    console.log(`   Upgrading '${artifactsName.WitnetProxy}' instance at ${WitnetProxy.address}:\n`)
+    await proxy.upgradeTo(
+      WitnetRequestBoard.address,
+      web3.eth.abi.encodeParameter(
+        "address[]",
+        [accounts[0]]
+      )
+    )
+    console.log(`   >> WRB owner address:  ${await wrb.owner.call()}`)
+    if (isNullAddress(oldAddr)) {
+      console.log(`   >> WRB address:        ${await proxy.implementation.call()}`)
+      console.log(`   >> WRB proxiableUUID:  ${await wrb.proxiableUUID.call()}`)
+      console.log(`   >> WRB codehash:       ${await wrb.codehash.call()}`)
+      console.log(`   >> WRB version tag:    ${web3.utils.hexToString(await wrb.version.call())}`)
+    } else {
+      console.log(`   >> WRB addresses:      ${oldAddr} => ${await proxy.implementation.call()}`)
+      console.log(`   >> WRB proxiableUUID:  ${await wrb.proxiableUUID.call()}`)
+      console.log(`   >> WRB codehashes:     ${oldCodehash} => ${await wrb.codehash.call()}`)
+      console.log(
+        `   >> WRB version tags:   '${web3.utils.hexToString(oldVersion)}'`,
+        `=> '${web3.utils.hexToString(await wrb.version.call())}'`
+      )
+    }
+    console.log(`   >> WRB soft-upgradable:${await wrb.isUpgradable.call()}\n`)
+  } else {
+    console.log(`\n   Skipped: '${artifactsName.WitnetRequestBoard}' deployed at ${WitnetRequestBoard.address}.`)
+  }
+}
+
+function isNullAddress (addr) {
+   return !addr ||
+      addr === "0x0000000000000000000000000000000000000000" ||
+      !web3.utils.isAddress(addr)
+}

scripts/migrate.js
@@ -1,0 +1,71 @@
+/* eslint-disable camelcase */
+/* eslint-disable new-cap */
+/* eslint-disable no-multi-str */
+/* eslint-disable no-template-curly-in-string */
+
+require("dotenv").config()
+
+const settings = require("../migrations/settings.witnet")
+const realm = process.env.WITNET_EVM_REALM || "default"
+
+const exec = require("child_process").exec
+const fs = require("fs")
+const cli = new cli_func()
+
+if (process.argv.length < 3) {
+  console.log()
+  console.log("\n\
+    Usage: yarn migrate <Network>\n\
+       or: npm run migrate <Network>\n\n\
+  ")
+  process.exit(0)
+}
+
+const network = process.argv[2]
+if (!settings.networks[realm][network]) {
+  console.error(`\n!!! Network "${realm}:${network}" not found.\n`)
+  console.error(`> Available networks in realm "${realm}":`)
+  console.error(settings.networks[realm])
+  process.exit(0)
+}
+
+const artifact = settings.artifacts[realm]
+  && settings.artifacts[realm].WitnetRequestBoard
+  || settings.artifacts.default.WitnetRequestBoard
+process.env.FLATTENED_DIRECTORY = `./flattened/${artifact}/`
+if (!fs.existsSync(`${process.env.FLATTENED_DIRECTORY}/Flattened${artifact}.sol`)) {
+  console.log("\n\
+    > Please, flatten Witnet artifacts first. E.g.:\n\
+      $ yarn flatten:witnet\n\n\
+  ")
+  process.exit(0)
+}
+
+migrateFlattened(network)
+
+/// /////////////////////////////////////////////////////////////////////////////
+
+async function migrateFlattened (network) {
+  console.log(
+    `> Migrating from ${process.env.FLATTENED_DIRECTORY} into "${realm}:${network}"..."`
+  )    
+  await cli.exec(`truffle migrate --reset --config truffle-config.flattened.js --network ${network}`)
+    .catch(err => {
+      console.error(err)
+      process.exit(1)
+    })
+}
+
+function cli_func () {
+  this.exec = async function (cmd) {
+    return new Promise((resolve, reject) => {
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          reject(error)
+          return
+        }
+        resolve(stdout)
+      }).stdout.pipe(process.stdout)
+    })
+  }
+}

witnet/witnet-solidity-bridge

migrations/scripts/1_deploy_wrb.js
@@ -1,0 +1,139 @@
+const { merge } = require("lodash")
+
+const realm = process.env.WITNET_EVM_REALM ? process.env.WITNET_EVM_REALM.toLowerCase() : "default"
+const settings = require("../witnet.settings")
+const artifactsName = merge(settings.artifacts.default, settings.artifacts[realm])
+
+module.exports = async function (deployer, network, accounts) {
+  const addresses = require("../witnet.addresses")[realm][network.split("-")[0]]
+
+  let WitnetParserLib, WitnetDecoderLib
+  let WitnetProxy, WitnetRequestBoard
+  let upgradeProxy = false
+
+  /* Load the 'WitnetProxy' artifact only if neccesary, and deploy it if not done yet */
+  try {
+    WitnetProxy = artifacts.require(artifactsName.WitnetProxy)
+    if (!WitnetProxy.isDeployed() || WitnetProxy.address !== addresses.WitnetRequestBoard) {
+      // If the 'WitnetProxy' artifact is found, we assume that the
+      // target 'WitnetRequestBoard' implementation is proxiable. In this case,
+      // the 'WitnetProxy' actual address will be read from the one assigned to the
+      // 'WitnetRequestBoard' artifact in the addresses file:
+      if (addresses) WitnetProxy.address = addresses.WitnetRequestBoard
+    }
+    if (!WitnetProxy.isDeployed() || isNullAddress(WitnetProxy.address)) {
+      await deployer.deploy(WitnetProxy)
+    } else {
+      console.log(`\n   Skipped: '${artifactsName.WitnetProxy}' deployed at ${WitnetProxy.address}.`)
+    }
+    upgradeProxy = true
+  } catch {
+    // If no 'WitnetProxy' artifact is found, we assume that
+    // the target 'WitnetRequestBoard' implementation is not proxiable.
+  }
+
+  /* Load target 'WitnetRequestBoard' implementation artifact */
+  try {
+    WitnetRequestBoard = artifacts.require(artifactsName.WitnetRequestBoard)
+  } catch {
+    console.log(`\n   Skipped: '${artifactsName.WitnetRequestBoard}' artifact not found.`)
+    return
+  }
+
+  if (!upgradeProxy) {
+    if (!WitnetRequestBoard.isDeployed() || isNullAddress(WitnetRequestBoard.address)) {
+      // Read implementation address from file only if the implementation requires no proxy
+      if (addresses) WitnetRequestBoard.address = addresses.WitnetRequestBoard
+    }
+  }
+
+  /* Try to find 'WitnetParserLib' artifact, and deployed address if any */
+  try {
+    WitnetParserLib = artifacts.require(artifactsName.WitnetParserLib)
+    if (!WitnetParserLib.isDeployed() || WitnetParserLib.address !== addresses.WitnetParserLib) {
+      // If the 'WitnetParserLib' is found, try to read deployed address from addresses file:
+      if (addresses) WitnetParserLib.address = addresses.WitnetParserLib
+    }
+  } catch {
+    console.error(`\n   Fatal: '${artifactsName.WitnetParserLib}' artifact not found.\n`)
+    process.exit(1)
+  }
+
+  /* Deploy new instance of 'WitnetParserLib', and 'WitnetDecoderLib', if neccesary */
+  if (!WitnetParserLib.isDeployed() || isNullAddress(WitnetParserLib.address)) {
+    // Fetch the 'WitnetDecoderLib' artifact:
+    try {
+      WitnetDecoderLib = artifacts.require(artifactsName.WitnetDecoderLib)
+    } catch {
+      console.error(`\n   Fatal: '${artifactsName.WitnetDecoderLib}' artifact not found.\n`)
+      process.exit(1)
+    }
+    // Deploy the 'WitnetDecoderLib' artifact first, if not done yet:
+    if (!WitnetDecoderLib.isDeployed()) {
+      await deployer.deploy(WitnetDecoderLib)
+      // and link the just-deployed 'WitnetDecoderLib' to the 'WitnetParserLib' artifact:
+      await deployer.link(WitnetDecoderLib, WitnetParserLib)
+    }
+    await deployer.deploy(WitnetParserLib)
+  } else {
+    console.log(`\n   Skipped: '${artifactsName.WitnetParserLib}' deployed at ${WitnetParserLib.address}.`)
+  }
+
+  /* Deploy new instance of target 'WitnetRequestBoard' implementation */
+  await deployer.link(WitnetParserLib, WitnetRequestBoard)
+  await deployer.deploy(
+    WitnetRequestBoard,
+    ...(
+    // use realm-specific constructor parameters, if defined...
+      settings.constructorParams[realm] && settings.constructorParams[realm].WitnetRequestBoard
+        ? settings.constructorParams[realm].WitnetRequestBoard
+        : settings.constructorParams.default.WitnetRequestBoard
+    )
+  )
+
+  /* Upgrade 'WitnetProxy' instance, if neccesary */
+  if (upgradeProxy) {
+    const proxy = await WitnetProxy.deployed()
+    const wrb = await WitnetRequestBoard.deployed()
+
+    const oldAddr = await proxy.implementation.call()
+    let oldCodehash, oldVersion
+    if (!isNullAddress(oldAddr)) {
+      const oldWrb = await WitnetRequestBoard.at(oldAddr)
+      oldCodehash = await oldWrb.codehash.call()
+      oldVersion = await oldWrb.version.call()
+    }
+    console.log(`   Upgrading '${artifactsName.WitnetProxy}' instance at ${WitnetProxy.address}:\n`)
+    await proxy.upgradeTo(
+      WitnetRequestBoard.address,
+      web3.eth.abi.encodeParameter(
+        "address[]",
+        [accounts[0]]
+      )
+    )
+    console.log(`   >> WRB owner address:  ${await wrb.owner.call()}`)
+    if (isNullAddress(oldAddr)) {
+      console.log(`   >> WRB address:        ${await proxy.implementation.call()}`)
+      console.log(`   >> WRB proxiableUUID:  ${await wrb.proxiableUUID.call()}`)
+      console.log(`   >> WRB codehash:       ${await wrb.codehash.call()}`)
+      console.log(`   >> WRB version tag:    ${web3.utils.hexToString(await wrb.version.call())}`)
+    } else {
+      console.log(`   >> WRB addresses:      ${oldAddr} => ${await proxy.implementation.call()}`)
+      console.log(`   >> WRB proxiableUUID:  ${await wrb.proxiableUUID.call()}`)
+      console.log(`   >> WRB codehashes:     ${oldCodehash} => ${await wrb.codehash.call()}`)
+      console.log(
+        `   >> WRB version tags:   '${web3.utils.hexToString(oldVersion)}'`,
+        `=> '${web3.utils.hexToString(await wrb.version.call())}'`
+      )
+    }
+    console.log(`   >> WRB soft-upgradable:${await wrb.isUpgradable.call()}\n`)
+  } else {
+    console.log(`\n   Skipped: '${artifactsName.WitnetRequestBoard}' deployed at ${WitnetRequestBoard.address}.`)
+  }
+}
+
+function isNullAddress (addr) {
+  return !addr ||
+      addr === "0x0000000000000000000000000000000000000000" ||
+      !web3.utils.isAddress(addr)
+}

scripts/migrate.js
@@ -1,0 +1,70 @@
+/* eslint-disable camelcase */
+/* eslint-disable new-cap */
+/* eslint-disable no-multi-str */
+/* eslint-disable no-template-curly-in-string */
+
+require("dotenv").config()
+
+const settings = require("../migrations/witnet.settings")
+const realm = process.env.WITNET_EVM_REALM || "default"
+
+const exec = require("child_process").exec
+const fs = require("fs")
+const cli = new cli_func()
+
+if (process.argv.length < 3) {
+  console.log()
+  console.log("\n\
+    Usage: yarn migrate <Network>\n\
+       or: npm run migrate <Network>\n\n\
+  ")
+  process.exit(0)
+}
+
+const network = process.argv[2]
+if (!settings.networks[realm][network]) {
+  console.error(`\n!!! Network "${realm}:${network}" not found.\n`)
+  console.error(`> Available networks in realm "${realm}":`)
+  console.error(settings.networks[realm])
+  process.exit(0)
+}
+
+const artifact = (settings.artifacts[realm] && settings.artifacts[realm].WitnetRequestBoard) ||
+  settings.artifacts.default.WitnetRequestBoard
+process.env.FLATTENED_DIRECTORY = `./flattened/${artifact}/`
+if (!fs.existsSync(`${process.env.FLATTENED_DIRECTORY}/Flattened${artifact}.sol`)) {
+  console.log("\n\
+    > Please, flatten Witnet artifacts first. E.g.:\n\
+      $ yarn flatten:witnet\n\n\
+  ")
+  process.exit(0)
+}
+
+migrateFlattened(network)
+
+/// /////////////////////////////////////////////////////////////////////////////
+
+async function migrateFlattened (network) {
+  console.log(
+    `> Migrating from ${process.env.FLATTENED_DIRECTORY} into "${realm}:${network}"..."`
+  )
+  await cli.exec(`truffle migrate --reset --config truffle-config.flattened.js --network ${network}`)
+    .catch(err => {
+      console.error(err)
+      process.exit(1)
+    })
+}
+
+function cli_func () {
+  this.exec = async function (cmd) {
+    return new Promise((resolve, reject) => {
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          reject(error)
+          return
+        }
+        resolve(stdout)
+      }).stdout.pipe(process.stdout)
+    })
+  }
+}