e83a9de9c461c3868889cd1e884dbae52420669e
Guillermo Díaz <guillermo@otherplane.com>
2021-09-16 17:08:55 +0200

    chore: last PR arrangements

--- migrations/scripts/1_deploy_wrb.js
+++ migrations/scripts/1_deploy_wrb.js
@@ -88,11 +88,15 @@ module.exports = async function (deployer, network, accounts) {
     WitnetRequestBoard,
     ...(
       // if defined, use network-specific constructor parameters:
-      settings.constructorParams[network]?.WitnetRequestBoard
-        // otherwise, use realm-specific parameters, if any:
-        ?? settings.constructorParams[realm]?.WitnetRequestBoard 
-        // or, default defined parameters for WRBs, if any:
-        ?? settings.constructorParams.default?.WitnetRequestBoard
+      settings.constructorParams[network] && settings.constructorParams[network].WitnetRequestBoard
+        ? settings.constructorParams[network].WitnetRequestBoard
+      // otherwise, use realm-specific parameters, if any:
+        : settings.constructorParams[realm] && settings.constructorParams[realm].WitnetRequestBoard
+          ? settings.constructorParams[realm].WitnetRequestBoard
+          // or, default defined parameters for WRBs, if any:
+          : settings.constructorParams.default
+            ? settings.constructorParams.default.WitnetRequestBoard
+            : null
     )
   )
 
