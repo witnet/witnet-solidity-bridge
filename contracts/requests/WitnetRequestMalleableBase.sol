2c05b9cf9727ea2375e77afa8699d2933d5d28ff
Guillermo Díaz <guillermo@otherplane.com>
2021-11-08 14:12:46 +0100

    refactor: turn WitnetRequestMalleableBase into a Proxiable contract. As to ease inheritance in upgradable contracts.

--- contracts/requests/WitnetRequestMalleableBase.sol
+++ contracts/requests/WitnetRequestMalleableBase.sol
@@ -2,65 +2,97 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "./WitnetRequestBase.sol";
+import "../libs/Witnet.sol";
 import "../patterns/Ownable.sol";
+import "../patterns/Proxiable.sol";
 
 abstract contract WitnetRequestMalleableBase
     is
-        WitnetRequestBase,
-        Ownable
+        IWitnetRequest,
+        Ownable,
+        Proxiable
 {   
     using Witnet for *;
 
-    /// Contains immutable template of the request bytecode.
-    bytes public template;
+    struct RequestState {
+        /// Contract owner address.
+        address owner;
+        /// Immutable bytecode template.
+        bytes template;
+        /// Current request bytecode.
+        bytes bytecode;
+        /// Current request hash.
+        bytes32 hash;
+        /// Current request witnessing params.
+        RequestWitnessingParams params;
+    }
 
-    /// Number of witnesses required to be involved for solving this Witnet Data Request.
-    uint8 public numWitnesses;
+    struct RequestWitnessingParams {
+        /// Number of witnesses required to be involved for solving this Witnet Data Request.
+        uint8 numWitnesses;
 
-    /// Threshold percentage for aborting resolution of a request if the witnessing nodes did not arrive to a broad consensus.
-    uint8 public minWitnessingConsensus;
+        /// Threshold percentage for aborting resolution of a request if the witnessing nodes did not arrive to a broad consensus.
+        uint8 minWitnessingConsensus;
 
-    /// Amount of nanowits that a witness solving the request will be required to collateralize in the commitment transaction.
-    uint64 public witnessingCollateral;
+        /// Amount of nanowits that a witness solving the request will be required to collateralize in the commitment transaction.
+        uint64 witnessingCollateral;
 
-    /// Amount of nanowits that every request-solving witness will be rewarded with.
-    uint64 public witnessingReward;
+        /// Amount of nanowits that every request-solving witness will be rewarded with.
+        uint64 witnessingReward;
 
-    /// Amount of nanowits that will be earned by Witnet miners for each each valid commit/reveal transaction they include in a block.
-    uint64 public witnessingUnitaryFee;
-    
+        /// Amount of nanowits that will be earned by Witnet miners for each each valid commit/reveal transaction they include in a block.
+        uint64 witnessingUnitaryFee;
+    }
+
     constructor(bytes memory _template)
     {
         assert(_template.length > 0);
-        template = _template;
+        _state().template = _template;
 
-        numWitnesses = 2;
-        minWitnessingConsensus = 51;
-        witnessingCollateral = 10 ** 9;
+        _params().numWitnesses = 2;
+        _params().minWitnessingConsensus = 51;
+        _params().witnessingCollateral = 10 ** 9;
         
         _malleateBytecode(
-            numWitnesses,
-            minWitnessingConsensus,
-            witnessingCollateral,
+            _params().numWitnesses,
+            _params().minWitnessingConsensus,
+            _params().witnessingCollateral,
             0,
             0
         );
     }
 
-    /// Set amount of nanowits that a witness solving the request will be required to collateralize in the commitment transaction.
+    /// Returns current Witnet Data Request bytecode, encoded using Protocol Buffers.
+    function bytecode() external view override returns (bytes memory) {
+        return _state().bytecode;
+    }
+
+    /// Returns SHA256 hash of current Witnet Data Request bytecode.
+    function hash() external view override returns (bytes32) {
+        return _state().hash;
+    }
+
+    /// Returns witnessing parameters of current Witnet Data Request.
+    function params()
+        external view
+        returns (RequestWitnessingParams memory)
+    {
+        return _state().params;
+    }
+
+    /// Sets amount of nanowits that a witness solving the request will be required to collateralize in the commitment transaction.
     function setWitnessingCollateral(uint64 _witnessingCollateral)
         public
         virtual
         onlyOwner
     {
-        witnessingCollateral = _witnessingCollateral;
+        _params().witnessingCollateral = _witnessingCollateral;
         _malleateBytecode(
-            numWitnesses,
-            minWitnessingConsensus,
+            _params().numWitnesses,
+            _params().minWitnessingConsensus,
             _witnessingCollateral,
-            witnessingReward,
-            witnessingUnitaryFee
+            _params().witnessingReward,
+            _params().witnessingUnitaryFee
         );
     }
 
@@ -73,12 +105,12 @@ abstract contract WitnetRequestMalleableBase
         virtual
         onlyOwner
     {
-        witnessingReward = _witnessingReward;
-        witnessingUnitaryFee = _witnessingUnitaryFee;
+        _params().witnessingReward = _witnessingReward;
+        _params().witnessingUnitaryFee = _witnessingUnitaryFee;
         _malleateBytecode(
-            numWitnesses,
-            minWitnessingConsensus,
-            witnessingCollateral,
+            _params().numWitnesses,
+            _params().minWitnessingConsensus,
+            _params().witnessingCollateral,
             _witnessingReward,
             _witnessingUnitaryFee
         );
@@ -93,14 +125,14 @@ abstract contract WitnetRequestMalleableBase
         virtual
         onlyOwner
     {
-        numWitnesses = _numWitnesses;
-        minWitnessingConsensus = _minWitnessingConsensus;
+        _params().numWitnesses = _numWitnesses;
+        _params().minWitnessingConsensus = _minWitnessingConsensus;
         _malleateBytecode(
             _numWitnesses,
             _minWitnessingConsensus,
-            witnessingCollateral,
-            witnessingReward,
-            witnessingUnitaryFee
+            _params().witnessingCollateral,
+            _params().witnessingReward,
+            _params().witnessingUnitaryFee
         );
     }
 
@@ -109,7 +141,8 @@ abstract contract WitnetRequestMalleableBase
         external view
         returns (uint128)
     {
-        return numWitnesses * witnessingCollateral;
+        
+        return _params().numWitnesses * _params().witnessingCollateral;
     }
 
     /// Return total amount of nanowits that will have to be paid in total for this request to be solved.
@@ -117,11 +150,51 @@ abstract contract WitnetRequestMalleableBase
         external view
         returns (uint128)
     {
-        return numWitnesses * (2 * witnessingUnitaryFee + witnessingReward);
+        return _params().numWitnesses * (2 * _params().witnessingUnitaryFee + _params().witnessingReward);
     }
 
+
     // ================================================================================================================
-    // --- Internal functions -----------------------------------------------------------------------------------------    
+    // --- 'Ownable' overriden functions ------------------------------------------------------------------------------
+
+    /// Returns the address of the current owner.
+    function owner()
+        public view
+        virtual override
+        returns (address)
+    {
+        return _state().owner;
+    }
+
+    /// @dev Transfers ownership of the contract to a new account (`newOwner`).
+    /// Internal function without access restriction.
+    function _transferOwnership(address newOwner)
+        internal
+        virtual override
+    {
+        address oldOwner = _state().owner;
+        _state().owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+
+
+    // ================================================================================================================
+    // --- 'Proxiable 'overriden functions ----------------------------------------------------------------------------
+
+    function proxiableUUID()
+        external pure
+        virtual override
+        returns (bytes32)
+    {
+        return (
+            /* keccak256("io.witnet.requests.malleable") */
+            0x851d0a92a3ad30295bef33afc69d6874779826b7789386b336e22621365ed2c2
+        );
+    }
+
+
+    // ================================================================================================================
+    // --- INTERNAL FUNCTIONS -----------------------------------------------------------------------------------------    
 
     /// Serialize new `bytecode` by combining immutable template with given parameters.
     function _malleateBytecode(
@@ -147,15 +220,37 @@ abstract contract WitnetRequestMalleableBase
             "WitnetRequestMalleableBase: witnessing collateral below 1 WIT"
         );
 
-        bytecode = abi.encodePacked(
-            template,
+        _state().bytecode = abi.encodePacked(
+            _state().template,
             _witnessingReward > 0 ? _uint64varint(bytes1(0x10), _witnessingReward) : bytes(""),
             _uint8varint(bytes1(0x18), _numWitnesses),
             _uint64varint(0x20, _witnessingUnitaryFee),
             _uint8varint(0x28, _minWitnessingConsensus),
             _uint64varint(0x30, _witnessingCollateral)
         );
-        hash = bytecode.hash();
+        _state().hash = _state().bytecode.hash();
+    }
+
+    /// Returns pointer to storage slot where WitnessingParams struct is located.
+    function _params()
+        internal pure
+        virtual
+        returns (RequestWitnessingParams storage)
+    {
+        return _state().params;
+    }
+
+    /// Return pointer to storage slot where State struct is located.
+    function _state()
+        internal pure
+        virtual
+        returns (RequestState storage _ptr)
+    {
+        assembly {
+            _ptr.slot :=
+                /* keccak256("io.witnet.requests.malleable.state") */
+                0xf35ef70bf77c836ff490bec16a682d32deb0baa15e4ecd19280856af5e08c11c
+        }
     }
 
     /// Encode uint64 into tagged varint.
