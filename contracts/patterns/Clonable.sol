c63d32f9858a0df3242f43745e2e2d60205adee9
Guillermo Díaz <guillermo@otherplane.com>
2021-11-11 20:00:01 +0100

    chore(patterns): polish Clonable pattern

--- contracts/patterns/Clonable.sol
+++ contracts/patterns/Clonable.sol
@@ -5,7 +5,19 @@ pragma solidity >=0.6.0 <0.9.0;
 import "./Initializable.sol";
 
 abstract contract Clonable is Initializable {
-    address immutable private __self = address(this);
+    address immutable public self = address(this);
+
+    event Cloned(address indexed by, Clonable indexed clone);
+
+    /// Tells whether this contract is a clone of another (i.e. `self()`)
+    function cloned()
+        public view
+        returns (bool)
+    {
+        return (
+            address(this) != self
+        );
+    }
 
     /// Deploys and returns the address of a minimal proxy clone that replicates contract
     /// behaviour while using its own EVM storage.
@@ -15,7 +27,7 @@ abstract contract Clonable is Initializable {
         public virtual
         returns (Clonable _instance)
     {
-        address _self = __self;
+        address _self = self;
         assembly {
             let ptr := mload(0x40)
             mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
@@ -24,6 +36,7 @@ abstract contract Clonable is Initializable {
             _instance := create(0, ptr, 0x37)
         }        
         require(address(_instance) != address(0), "Clonable: CREATE failed");
+        emit Cloned(msg.sender, _instance);
     }
 
     /// Deploys and returns the address of a minimal proxy clone that replicates contract 
@@ -35,7 +48,7 @@ abstract contract Clonable is Initializable {
         public virtual
         returns (Clonable _instance)
     {
-        address _self = __self;
+        address _self = self;
         assembly {
             let ptr := mload(0x40)
             mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
@@ -44,5 +57,6 @@ abstract contract Clonable is Initializable {
             _instance := create2(0, ptr, 0x37, _salt)
         }
         require(address(_instance) != address(0), "Clonable: CREATE2 failed");
+        emit Cloned(msg.sender, _instance);
     }
 }
