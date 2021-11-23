fa4679275d36de521512393186996d7418f30b29
Guillermo Díaz <guillermo@otherplane.com>
2021-11-23 14:26:24 +0100

    chore(WitnetRNG): add verified addresses

--- migrations/scripts/1_deploy_wrb.js
+++ migrations/scripts/1_deploy_wrb.js
@@ -84,6 +84,31 @@ module.exports = async function (deployer, network, accounts) {
   }
 
   /* Deploy new instance of target 'WitnetRequestBoard' implementation */
+  if (upgradeProxy) {
+    // But ask operator first, if this was a proxiable implementation:
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
+    await new Promise((resolve) => {
+      rl.question(
+        "\n   > Do you wish to upgrade the proxy ? [y/N] ",
+        function (answer) {
+          if (!["y", "yes"].includes(answer.toLowerCase().trim())) {
+            upgradeProxy = false
+          }
+          rl.close()
+        })
+      rl.on("close", function () {
+        resolve()
+      })
+    })
+    if (!upgradeProxy) {
+      return
+    }
+  }
+
   await deployer.link(WitnetParserLib, WitnetRequestBoard)
   await deployer.deploy(
     WitnetRequestBoard,
