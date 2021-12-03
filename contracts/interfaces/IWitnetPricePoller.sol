0bbdf5ae44592511e44d677967d5c3a9dcdcc410
Guillermo Díaz <guillermo@otherplane.com>
2021-12-03 17:22:12 +0100

    feat: revisit IWitnetPricePoller

--- contracts/interfaces/IWitnetPricePoller.sol
+++ contracts/interfaces/IWitnetPricePoller.sol
@@ -8,10 +8,14 @@ interface IWitnetPricePoller {
 
     event PricePolling(address indexed from, uint256 queryId, uint256 extraFee);
 
+    function estimateUpdateFee(uint256) external view returns (uint256);
+
     function lastPrice() external view returns (int256);
     function lastTimestamp() external view returns (uint256);    
     function lastValue() external view returns (int, uint, bytes32);
 
+    function latestQueryId() external view returns (uint256);
+    function latestUpdateDrTxHash() external view returns (bytes32);
     function latestUpdateErrorMessage() external view returns (string memory);
     function latestUpdateStatus() external view returns (uint256);
 
