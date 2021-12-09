f5ef7d5cd85b8302ea198b210d2535371e169aa3
Guillermo Díaz <guillermo@otherplane.com>
2021-12-09 17:41:48 +0100

    docs: add comments to IWitnetPriceRouter interface, and implementation

--- contracts/examples/WitnetPriceRouter.sol
+++ contracts/examples/WitnetPriceRouter.sol
@@ -24,31 +24,66 @@ contract WitnetPriceRouter
         string  quote;
     }    
 
-    mapping (bytes32 => PricePair) internal _pairs;
-    mapping (address => bytes32) internal _feedId_;
+    mapping (bytes32 => PricePair) internal __pairs;
+    mapping (address => bytes32) internal __feedId_;
 
-    bytes32[] internal _pricePairs;
+    bytes32[] internal __supportedPricePairs;
+
+    // ========================================================================
+    // --- Implementation of 'IERC2362' ---------------------------------------
+
+    /// Returns last valid price value and timestamp, as well as status of
+    /// the latest update request that got posted to the Witnet Request Board. 
+    /// @dev Fails if the given price pair is not currently supported.
+    /// @param _erc2362id Price pair identifier as specified in https://github.com/adoracles/ADOIPs/blob/main/adoip-0010.md
+    /// @return _lastPrice Last valid price reported back from the Witnet oracle.
+    /// @return _lastTimestamp EVM-timestamp of the last valid price.
+    /// @return _latestUpdateStatus Status code of latest update request that got posted to the Witnet Request Board:
+    ///          - 200: latest update request was succesfully solved with no errors
+    ///          - 400: latest update request was solved with errors
+    ///          - 404: latest update request is still pending to be solved    
+	function valueFor(bytes32 _erc2362id)
+        external view
+        virtual override
+        returns (
+            int256 _lastPrice,
+            uint256 _lastTimestamp,
+            uint256 _latestUpdateStatus
+        )
+    {
+        IWitnetPriceFeed _feed = IWitnetPriceFeed(address(getPriceFeed(_erc2362id)));
+        require(address(_feed) != address(0), "WitnetPriceRouter: not currently supported");
+        return _feed.lastValue();
+    }
+
 
     // ========================================================================
     // --- Implementation of 'IWitnetPriceRouter' ---------------------------    
 
+    /// Returns the ERC-165-compliant price feed contract currently attending 
+    /// updates on the given price pair.
     function getPriceFeed(bytes32 _erc2362id)
         public view
         virtual override
         returns (IERC165)
     {
-        return _pairs[_erc2362id].feed;
+        return __pairs[_erc2362id].feed;
     }
 
+    /// Returns human-readable ERC2362-based caption of the price pair being
+    /// attended by the given price feed contract address. 
+    /// @dev Fails if the given price feed contract address is not currently
+    /// @dev supported by the router.
     function getPriceFeedCaption(IERC165 _feed) 
         public view
         virtual override
         returns (string memory)
     {
         require(supportsPriceFeed(_feed), "WitnetPriceRouter: unknown");
-        return lookupERC2362ID(_feedId_[address(_feed)]);
+        return lookupERC2362ID(__feedId_[address(_feed)]);
     }
 
+    /// Helper pure function: returns hash of the provided ERC2362-compliant price pair caption.
     function hashPriceCaption(string memory _caption)
         public pure
         virtual override
@@ -57,12 +92,13 @@ contract WitnetPriceRouter
         return keccak256(bytes(_caption));
     }
 
+    /// Returns human-readable caption of the ERC2362-based price pair identifier, if known.
     function lookupERC2362ID(bytes32 _erc2362id)
         public view
         virtual override
         returns (string memory _caption)
     {
-        PricePair storage _pair = _pairs[_erc2362id];
+        PricePair storage _pair = __pairs[_erc2362id];
         if (
             bytes(_pair.base).length > 0 
                 && bytes(_pair.quote).length > 0
@@ -78,6 +114,9 @@ contract WitnetPriceRouter
         }
     }
 
+    /// Sets price feed contract attached to given price pair identifier.
+    /// @dev Setting zero address to a price pair implies that it will not be attended any longer.
+    /// @dev Otherwise, fails if the price feed contract does not support the `IWitnetPriceFeed` interface.
     function setPriceFeed(
             IERC165 _feed,
             uint256 _decimals,
@@ -103,65 +142,48 @@ contract WitnetPriceRouter
             _decimals.toString()
         );
         bytes32 _erc2362id = keccak256(_caption);
-        PricePair storage _record = _pairs[_erc2362id];
+        PricePair storage _record = __pairs[_erc2362id];
         address _currentFeed = address(_record.feed);
         if (bytes(_record.base).length == 0) {
             _record.base = _base;
             _record.quote = _quote;
             _record.decimals = _decimals;
-            _pricePairs.push(_erc2362id);
+            __supportedPricePairs.push(_erc2362id);
         }
         else if (_currentFeed != address(0)) {
-            _feedId_[_currentFeed] = bytes32(0);
+            __feedId_[_currentFeed] = bytes32(0);
         }
         if (address(_feed) != _currentFeed) {
-            _feedId_[address(_feed)] = _erc2362id;
+            __feedId_[address(_feed)] = _erc2362id;
         }
         _record.feed = _feed;
         emit PricePairSet(_erc2362id, _feed);
     }
 
+    /// Returns list of known price pairs identifiers.
     function supportedPricePairs()
         external view
         virtual override
         returns (bytes32[] memory)
     {
-        return _pricePairs;
+        return __supportedPricePairs;
     }
 
+    /// Returns `true` if given price pair is currently attached to a compliant price feed contract.
     function supportsPricePair(bytes32 _erc2362id)
         public view
         virtual override
         returns (bool)
     {
-        return address(_pairs[_erc2362id].feed) != address(0);
+        return address(__pairs[_erc2362id].feed) != address(0);
     }
 
+    /// Returns `true` if given price feed contract is currently attached to a known price pair. 
     function supportsPriceFeed(IERC165 _feed)
         public view
         virtual override
         returns (bool)
     {
-        return _pairs[_feedId_[address(_feed)]].feed == _feed;
-    }
-
-
-    // ========================================================================
-    // --- Implementation of 'IERC2362' ---------------------------------------
-
-    /// Exposed function pertaining to EIP standards
-	/// @param _erc2362id bytes32 ID of the query
-	function valueFor(bytes32 _erc2362id)
-        external view
-        virtual override
-        returns (
-            int256 _lastPrice,
-            uint256 _lastTimestamp,
-            uint256 _latestUpdateStatus
-        )
-    {
-        IWitnetPriceFeed _feed = IWitnetPriceFeed(address(getPriceFeed(_erc2362id)));
-        require(address(_feed) != address(0), "WitnetPriceRouter: not currently supported");
-        return _feed.lastValue();
+        return __pairs[__feedId_[address(_feed)]].feed == _feed;
     }
 }
--- contracts/interfaces/IWitnetPriceRouter.sol
+++ contracts/interfaces/IWitnetPriceRouter.sol
@@ -4,16 +4,35 @@ pragma experimental ABIEncoderV2;
 
 import "./IERC165.sol";
 
+/// @title The Witnet Price Router basic interface.
+/// @dev Guides implementation of price feeds aggregation contracts.
+/// @author The Witnet Foundation.
 interface IWitnetPriceRouter
 {
+    /// Emitted everytime a price pair is attached to a new price feed contract
+    /// @dev See https://github.com/adoracles/ADOIPs/blob/main/adoip-0010.md 
+    /// @dev to learn how these ids are created.
     event PricePairSet(bytes32 indexed erc2362ID, IERC165 pricefeed);
 
+    /// Returns the ERC-165-compliant price feed contract currently attending 
+    /// updates on the given price pair.
     function getPriceFeed(bytes32 _erc2362id) external view returns (IERC165);
+
+    /// Returns human-readable ERC2362-based caption of the price pair being
+    /// attended by the given price feed contract address. 
+    /// @dev Should fail if the given price feed contract address is not currently
+    /// @dev supported by the router.
     function getPriceFeedCaption(IERC165 _priceFeed) external view returns (string memory);
 
+    /// Helper pure function: returns hash of the provided ERC2362-compliant price pair caption.
     function hashPriceCaption(string memory _priceCaption) external pure returns (bytes32);
+
+    /// Returns human-readable caption of the ERC2362-based price pair identifier, if known.
     function lookupERC2362ID(bytes32 _erc2362id) external view returns (string memory);
 
+    /// Sets price feed contract attached to given price pair identifier.
+    /// @dev Setting zero address to a price pair implies that it will not be attended any longer.
+    /// @dev Otherwise, it should fail if the price feed contract does not support the `IWitnetPriceFeed` interface.
     function setPriceFeed(
             IERC165 _priceFeed,
             uint256 _priceDecimals,
@@ -22,7 +41,12 @@ interface IWitnetPriceRouter
         )
         external;
 
+    /// Returns list of known price pairs identifiers.
     function supportedPricePairs() external view returns (bytes32[] memory);
+
+    /// Returns `true` if given price pair is currently attached to a compliant price feed contract.
     function supportsPricePair(bytes32 _erc2362id) external view returns (bool);
+
+    /// Returns `true` if given price feed contract is currently attached to a known price pair. 
     function supportsPriceFeed(IERC165 _priceFeed) external view returns (bool);
 }
