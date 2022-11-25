954c4d0c3cede214bc504496f41ade1e8a93deb4
Guillermo Díaz <guillermo@otherplane.com>
2022-11-25 10:55:38 +0100

    chore(v2): keep polishing interfaces and data structs

--- contracts/libs/WitnetV2.sol
+++ contracts/libs/WitnetV2.sol
@@ -9,8 +9,7 @@ library WitnetV2 {
     error IndexOutOfBounds(uint256 index, uint256 range);
     error InsufficientBalance(uint256 weiBalance, uint256 weiExpected);
     error InsufficientFee(uint256 weiProvided, uint256 weiExpected);
-    error Unauthorized(address violator);
-    
+    error Unauthorized(address violator);    
 
     function toEpoch(uint _timestamp) internal pure returns (uint) {
         return 1 + (_timestamp - 11111) / 15;
@@ -44,33 +43,13 @@ library WitnetV2 {
         Idle
     }
 
-    struct DrSla {
-        uint64 witnessReward;
-        uint16 numwitnesses;
-        uint64 commitRevelFee;
-        uint32 minConsensusPercentage;
-        uint64 collateral;        
-    }
-
     struct DrPost {
         uint256 block;
         DrPostStatus status;
         DrPostRequest request;
         DrPostResponse response;
     }
-
-    enum DrPostStatus {
-        Void,
-        Deleted,
-        Expired,
-        Posted,
-        Disputed,
-        Reported,
-        Finalized,
-        Accepted,
-        Rejected
-    }
-
+    
     /// Data kept in EVM-storage for every Request posted to the Witnet Request Board.
     struct DrPostRequest {
         uint256 epoch;
@@ -89,11 +68,108 @@ library WitnetV2 {
         uint256 drCommitTxEpoch;
         uint256 drTallyTxEpoch;
         bytes32 drTallyTxHash;
-        bytes   drTallyResultBytes;
+        bytes   drTallyResultCborBytes;
     }
 
-    enum Types {
-        Integer
+    enum DrPostStatus {
+        Void,
+        Deleted,
+        Expired,
+        Posted,
+        Disputed,
+        Reported,
+        Finalized,
+        Accepted,
+        Rejected
     }
+
+    struct DataProvider {
+        string  fqdn;
+        uint256 totalSources;
+        uint256 totalRetrievals;
+    }
+
+    enum DataRequestMethods {
+        /* 0 */ Unknown,
+        /* 1 */ HttpGet,
+        /* 2 */ Rng,
+        /* 3 */ HttpPost
+    }
+
+    struct DataSource {
+        DataRequestMethods method;
+        RadonDataTypes resultType;
+        string url;
+        string body;
+        string[2][] headers;
+        bytes script;
+    }
+
+    enum RadonDataTypes {
+        /* 0x0 */ Any, 
+        /* 0x1 */ Array,
+        /* 0x2 */ Bool,
+        /* 0x3 */ Bytes,
+        /* 0x4 */ Integer,
+        /* 0x5 */ Float,
+        /* 0x6 */ Map,
+        /* 0x7 */ String
+    }
+
+    struct RadonFilter {
+        RadonFilterOpcodes op;
+        bytes cborArgs;
+    }
+
+    enum RadonFilterOpcodes {
+        /* 0x00 */ GreaterThan,
+        /* 0x01 */ LessThan,
+        /* 0x02 */ Equals,
+        /* 0x03 */ AbsoluteDeviation,
+        /* 0x04 */ RelativeDeviation,
+        /* 0x05 */ StandardDeviation,
+        /* 0x06 */ Top,
+        /* 0x07 */ Bottom,
+        /* 0x08 */ Mode,
+        /* 0x09 */ LessOrEqualThan
+    }
+
+    struct RadonReducer {
+        RadonReducerOpcodes op;
+        RadonFilter[] filters;
+    }
+
+    enum RadonReducerOpcodes {
+        /* 0x00 */ Minimum,
+        /* 0x01 */ Maximum,
+        /* 0x02 */ Mode,
+        /* 0x03 */ AverageMean,
+        /* 0x04 */ AverageMeanWeighted,
+        /* 0x05 */ AverageMedian,
+        /* 0x06 */ AverageMedianWeighted,
+        /* 0x07 */ StandardDeviation,
+        /* 0x08 */ AverageDeviation,
+        /* 0x09 */ MedianDeviation,
+        /* 0x0A */ MaximumDeviation,
+        /* 0x0B */ ConcatenateAndHash
+    }
+
+    struct RadonSLA {
+        uint64 witnessReward;
+        uint16 numWitnesses;
+        uint64 commitRevealFee;
+        uint32 minConsensusPercentage;
+        uint64 collateral;
+    }
+
+    
+    // // ================================================================================================================
+    // // --- Internal view/pure methods ---------------------------------------------------------------------------------
+
+    // /// @notice Witnet function that computes the hash of a CBOR-encoded Data Request.
+    // /// @param _bytecode CBOR-encoded RADON.
+    // function hash(bytes memory _bytecode) internal pure returns (bytes32) {
+    //     return sha256(_bytecode);
+    // }
 
 }
