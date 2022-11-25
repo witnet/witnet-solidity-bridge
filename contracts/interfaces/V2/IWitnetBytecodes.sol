45e277c712d68635b63f172efdb2695b567fd841
Guillermo Díaz <guillermo@otherplane.com>
2022-11-25 11:22:50 +0100

    feat: first implementation approach to WitnetBytecodes

--- contracts/interfaces/V2/IWitnetBytecodes.sol
+++ contracts/interfaces/V2/IWitnetBytecodes.sol
@@ -5,29 +5,70 @@ pragma solidity >=0.8.0 <0.9.0;
 import "../../libs/WitnetV2.sol";
 
 interface IWitnetBytecodes {
+
+    struct RadonRetrieval {
+        WitnetV2.RadonDataTypes resultType;
+        uint16 resultMaxSize;
+        string[][] args;
+        bytes32[] sources;
+        bytes32 aggregator;
+        bytes32 tally;
+    }
     
-    event DrQueryHash(bytes32 drQueryHash);
-    event DrRadonHash(bytes32 drRadonHash);
-    event DrRadonTemplateHash(bytes32 drRadonTemplateHash, WitnetV2.Types[] types);
-    event DrSlaHash(bytes32 drSlaHash);
+    error RadonRetrievalNoSources();
+    error RadonRetrievalArgsMismatch(string[][] args);
+    error RadonRetrievalResultsMismatch(uint8 read, uint8 expected);
 
-    function bytecodeOf(bytes32 _drRadonHash) external view returns (bytes memory);
-    function bytecodeOf(bytes32 _drRadonHash, bytes32 _drSlaHash) external view returns (bytes memory);
-    function bytecodeOf(bytes32 _drRadonHash, bytes[] calldata _drArgs) external view returns (bytes memory);
-    function bytecodeOf(bytes32 _drRadonHash, bytes32 _drSlaHash, bytes[] calldata _drArgs) external view returns (bytes memory);
+    error RadonSlaNoReward();
+    error RadonSlaNoWitnesses();
+    error RadonSlaTooManyWitnesses(uint256 numWitnesses);
+    error RadonSlaConsensusOutOfRange(uint256 percentage);
+    error RadonSlaLowCollateral(uint256 collateral);
 
-    function hashOf(bytes32 _drRadonHash, bytes32 _drSlaHash) external pure returns (bytes32);
-    function hashOf(bytes32 _drRadonHash, bytes32 _drSlaHash, bytes[] calldata _drArgs) external view returns (bytes32);
+    error UnsupportedDataRequestMethod(uint8 method, string schema);
+    error UnsupportedDataRequestHeaders(string[2][] headers);
+    error UnsupportedRadonDataType(uint8 datatype, uint256 maxlength);
+    error UnsupportedRadonFilter(uint8 filter, bytes args);
+    error UnsupportedRadonReducer(uint8 reducer);
+    error UnsupportedRadonScript(bytes script, uint256 offset);
+    error UnsupportedRadonScriptOpcode(uint8 opcode);
 
-    function lookupDrSla(bytes32 _drSlaHash) external view returns (WitnetV2.DrSla memory);
-    function lookupDrSlaReward(bytes32 _drSlaHash) external view returns (uint256);
-    function lookupDrSources(bytes32 _drRadHash) external view returns (string[] memory);
-    function lookupDrInputTypes(bytes32 _drRadonHash) external view returns (WitnetV2.Types[] memory);
-    function lookupDrResultSize(bytes32 _drRadonHash) external view returns (uint256);
-    function lookupDrResultType(bytes32 _drRadonHash) external view returns (WitnetV2.Types);
+    event NewDataProvider(string fqdn, uint256 index);
+    event NewDataReducerHash(bytes32 hash);
+    event NewDataSourceHash(bytes32 hash, string url);
+    event NewRadonRetrievalHash(bytes32 hash);
+    event NewDrSlaHash(bytes32 hash);
 
-    function verifyDrRadonBytes(bytes calldata _drRadonBytes) external returns (bytes32 _drRadonHash);
-    function verifyDrRadonTemplateBytes(bytes calldata _drRadonBytes, WitnetV2.Types[] calldata _types) external returns (bytes32 _drRadonHash);
-    function verifyDrSla(WitnetV2.DrSla calldata _drSla) external returns (bytes32 _drSlaHash);
+    function bytecodeOf(bytes32 _drRetrievalHash) external view returns (bytes memory);
+    function bytecodeOf(bytes32 _drRetrievalHash, bytes32 _drSlaHash) external view returns (bytes memory);
 
+    function hashOf(bytes32 _drRetrievalHash, bytes32 _drSlaHash) external pure returns (bytes32 _drQueryHash);
+
+    function lookupDataProvider(uint256) external view returns (WitnetV2.DataProvider memory);
+    function lookupDataProviderIndex(string calldata) external view returns (uint);
+    function lookupDataSource(bytes32 _drDataSourceHash) external view returns (WitnetV2.DataSource memory);
+    function lookupRadonRetrievalAggregatorHash(bytes32 _drRetrievalHash) external view returns (WitnetV2.RadonReducer memory);
+    function lookupRadonRetrievalResultMaxSize(bytes32 _drRetrievalHash) external view returns (uint256);
+    function lookupRadonRetrievalResultType(bytes32 _drRetrievalHash) external view returns (WitnetV2.RadonDataTypes);
+    function lookupRadonRetrievalSourceHashes(bytes32 _drRetrievalHash) external view returns (bytes32[] memory);
+    function lookupRadonRetrievalSourcesCount(bytes32 _drRetrievalHash) external view returns (uint);
+    function lookupRadonRetrievalTallyHash(bytes32 _drRetrievalHash) external view returns (WitnetV2.RadonReducer memory);
+    function lookupRadonSLA(bytes32 _drSlaHash) external view returns (WitnetV2.RadonSLA memory);
+    function lookupRadonSLAReward(bytes32 _drSlaHash) external view returns (uint64);
+    
+    function verifyDataSource(
+            WitnetV2.DataRequestMethods _requestMethod,
+            string calldata _requestSchema,
+            string calldata _requestFQDN,
+            string calldata _requestPathQuery,
+            string calldata _requestBody,
+            string[2][] calldata _requestHeaders,
+            bytes calldata _witnetScript
+        ) external returns (bytes32);
+    function verifyRadonReducer(WitnetV2.RadonReducer calldata) external returns (bytes32);
+    function verifyRadonRetrieval(RadonRetrieval calldata) external returns (bytes32);
+    function verifyRadonSLA(WitnetV2.RadonSLA calldata _drSla) external returns (bytes32);
+
+    function totalDataProviders() external view returns (uint);
+   
 }
