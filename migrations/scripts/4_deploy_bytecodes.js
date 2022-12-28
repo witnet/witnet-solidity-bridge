79e50184b6051a3f9907ea48be81475176e0ae18
Guillermo Díaz <guillermo@otherplane.com>
2022-12-28 15:25:55 +0100

    chore: add migration script for WitnetBytecodes, proxy and encoding lib

---
+++ migrations/scripts/4_deploy_bytecodes.js
@@ -0,0 +1,90 @@
+const fs = require("fs")
+const { merge } = require("lodash")
+
+const addresses = require("../witnet.addresses")
+const package = require ("../../package")
+const settings = require("../witnet.settings")
+const utils = require("../../scripts/utils")
+
+const WitnetBytecodesProxy = artifacts.require("WitnetProxy")
+const WitnetBytecodesImplementation = artifacts.require("WitnetBytecodes")
+const WitnetEncodingLib = artifacts.require("WitnetEncodingLib")
+
+module.exports = async function (deployer, network, accounts) {
+  if (network !== "test") {
+    const isDryRun = network.split("-")[1] === "fork" || network.split("-")[0] === "develop"
+    const ecosystem = utils.getRealmNetworkFromArgs()[0]
+    network = network.split("-")[0]
+
+    if (!addresses[ecosystem]) addresses[ecosystem] = {}
+    if (!addresses[ecosystem][network]) addresses[ecosystem][network] = {}
+
+    console.info()
+
+    var proxy
+    if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetBytecodes)) {
+      await deployer.deploy(WitnetBytecodesProxy)
+      proxy = await WitnetBytecodesProxy.deployed()
+      addresses[ecosystem][network].WitnetBytecodes = proxy.address
+      if (!isDryRun) {
+        saveAddresses(addresses)
+      }
+    } else {
+      proxy = await WitnetBytecodesProxy.at(addresses[ecosystem][network].WitnetBytecodes)
+      console.info(`   Skipped: 'WitnetBytecodesProxy' deployed at ${proxy.address}`)
+    }
+
+    var bytecodes
+    if (
+      utils.isNullAddress(addresses[ecosystem][network]?.WitnetBytecodesImplementation)
+        || utils.isNullAddress(addresses[ecosystem][network]?.WitnetEncodingLib)
+    ) {
+      if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetEncodingLib)) {
+        await deployer.deploy(WitnetEncodingLib)
+        await deployer.link(WitnetEncodingLib, [WitnetBytecodesImplementation])
+        addresses[ecosystem][network].WitnetEncodingLib = WitnetEncodingLib.address
+        if (!isDryRun) {
+          saveAddresses(addresses)
+        }
+      } else {
+        console.info(`   Skipped: 'WitnetEncodingLib' presumably deployed at ${addresses[ecosystem][network].WitnetEncodingLib}`)
+      }
+      await deployer.deploy(
+        WitnetBytecodesImplementation,
+        true,
+        utils.fromAscii(package.version)
+      )
+      bytecodes = await WitnetBytecodesImplementation.deployed()
+      addresses[ecosystem][network].WitnetBytecodesImplementation = bytecodes.address
+      if (!isDryRun) {
+        saveAddresses(addresses)
+      }
+    } else {
+      bytecodes = await WitnetBytecodesImplementation.at(addresses[ecosystem][network].WitnetBytecodesImplementation)
+      console.info(`   Skipped: 'WitnetBytecodesImplementation' deployed at ${bytecodes.address}`)
+    }
+
+    var implementation = await proxy.implementation()
+    if (implementation.toLowerCase() !== bytecodes.address.toLowerCase()) {
+      console.info()
+      console.info("   > WitnetBytecodesImplementation:", bytecodes.address, `(v${await bytecodes.version()})`)
+      console.info("   > WitnetBytecodesProxy:", proxy.address)
+      console.info("   > WitnetBytecodesProxy.implementation::", implementation)
+      const answer = await utils.prompt(`   > Upgrade the proxy ? [y/N] `)
+      if (["y", "yes"].includes(answer.toLowerCase().trim())) {
+        await proxy.upgradeTo(bytecodes.address, "0x")
+        console.info("   > Done.")
+      } else {
+        console.info("   > Not upgraded.")
+      }
+    }
+  }
+}
+
+function saveAddresses(addrs) {
+  fs.writeFileSync(
+    "./migrations/witnet.addresses.json",
+    JSON.stringify(addrs, null, 4),
+    { flag: 'w+'}
+  )
+}
