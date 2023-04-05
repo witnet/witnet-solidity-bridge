021609cce43d48fadce2fd060e5721467a597dd4
Guillermo Díaz <guillermo@otherplane.com>
2023-04-05 20:47:17 +0200

    chore: let WitnetRandomness WitnetPriceRouter be deployed w/o a proxy

--- migrations/scripts/6_WitnetRandomness.js
+++ migrations/scripts/6_WitnetRandomness.js
@@ -25,96 +25,118 @@ module.exports = async function (deployer, network, [, from]) {
     return;
   }
 
-  let proxy
-  if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetRandomness)) {
-    var create2Factory = await Create2Factory.deployed()
-    if(
-      create2Factory && !utils.isNullAddress(create2Factory.address)
-        && singletons?.WitnetRandomness
-    ) {
-      // Deploy the proxy via a singleton factory and a salt...
-      const bytecode = WitnetRandomness.toJSON().bytecode
-      const salt = singletons.WitnetRandomness?.salt 
-        ? "0x" + ethUtils.setLengthLeft(
-            ethUtils.toBuffer(
-              singletons.WitnetRandomness.salt
-            ), 32
-          ).toString("hex")
-        : "0x0"
-      ;
-      const proxyAddr = await create2Factory.determineAddr.call(bytecode, salt, { from })
-      if ((await web3.eth.getCode(proxyAddr)).length <= 3) {
-        // deploy instance only if not found in current network:
-        utils.traceHeader(`Singleton inception of 'WitnetPriceRouter':`)
-        const balance = await web3.eth.getBalance(from)
-        const gas = singletons.WitnetRandomness.gas || 10 ** 6
-        const tx = await create2Factory.deploy(bytecode, salt, { from, gas })
-        utils.traceTx(
-          tx.receipt,
-          web3.utils.fromWei((balance - await web3.eth.getBalance(from)).toString())
-        )
+  if (addresses[ecosystem][network].WitnetRandomnessImplementation != undefined) {
+    let proxy
+    if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetRandomness)) {
+      var create2Factory = await Create2Factory.deployed()
+      if(
+        create2Factory && !utils.isNullAddress(create2Factory.address)
+          && singletons?.WitnetRandomness
+      ) {
+        // Deploy the proxy via a singleton factory and a salt...
+        const bytecode = WitnetRandomness.toJSON().bytecode
+        const salt = singletons.WitnetRandomness?.salt 
+          ? "0x" + ethUtils.setLengthLeft(
+              ethUtils.toBuffer(
+                singletons.WitnetRandomness.salt
+              ), 32
+            ).toString("hex")
+          : "0x0"
+        ;
+        const proxyAddr = await create2Factory.determineAddr.call(bytecode, salt, { from })
+        if ((await web3.eth.getCode(proxyAddr)).length <= 3) {
+          // deploy instance only if not found in current network:
+          utils.traceHeader(`Singleton inception of 'WitnetPriceRouter':`)
+          const balance = await web3.eth.getBalance(from)
+          const gas = singletons.WitnetRandomness.gas || 10 ** 6
+          const tx = await create2Factory.deploy(bytecode, salt, { from, gas })
+          utils.traceTx(
+            tx.receipt,
+            web3.utils.fromWei((balance - await web3.eth.getBalance(from)).toString())
+          )
+        } else {
+          utils.traceHeader(`Singleton 'WitnetRandomness':`)
+        }
+        console.info("  ", "> proxy address:       ", proxyAddr)
+        console.info("  ", "> proxy codehash:      ", web3.utils.soliditySha3(await web3.eth.getCode(proxyAddr)))        
+        console.info("  ", "> proxy inception salt:", salt)
+        proxy = await WitnetRandomness.at(proxyAddr)
       } else {
-        utils.traceHeader(`Singleton 'WitnetRandomness':`)
+        // Deploy no singleton proxy ...
+        await deployer.deploy(WitnetRandomness, { from })
+        proxy = await WitnetRandomness.deployed()
       }
-      console.info("  ", "> proxy address:       ", proxyAddr)
-      console.info("  ", "> proxy codehash:      ", web3.utils.soliditySha3(await web3.eth.getCode(proxyAddr)))        
-      console.info("  ", "> proxy inception salt:", salt)
-      proxy = await WitnetRandomness.at(proxyAddr)
-    } else {
-      // Deploy no singleton proxy ...
-      await deployer.deploy(WitnetRandomness, { from })
-      proxy = await WitnetRandomness.deployed()
-    }
-    addresses[ecosystem][network].WitnetRandomness = proxy.address
-    if (!isDryRun) {
-      utils.saveAddresses(addresses)
-    }
-  } else {
-    proxy = await WitnetRandomness.at(addresses[ecosystem][network].WitnetRandomness)
-    console.info(`   Skipped: 'WitnetRandomness' deployed at ${proxy.address}`)
-  }
-
-  var randomness
-  if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetRandomnessImplementation)) {
-    await deployer.deploy(
-      WitnetRandomnessImplementation,
-      WitnetRequestBoard.address,
-      true,
-      utils.fromAscii(packageJson.version),
-      { from }
-    )
-    randomness = await WitnetRandomnessImplementation.deployed()
-    addresses[ecosystem][network].WitnetRandomnessImplementation = randomness.address
-    if (!isDryRun) {
-      utils.saveAddresses(addresses)
-    }
-  } else {
-    randomness = await WitnetRandomnessImplementation.at(addresses[ecosystem][network].WitnetRandomnessImplementation)
-    console.info(`   Skipped: 'WitnetRandomnessImplementation' deployed at ${randomness.address}`)
-  }
-
-  const implementation = await proxy.implementation()
-  if (implementation.toLowerCase() !== randomness.address.toLowerCase()) {
-    console.info()
-    console.info("   > WitnetRandomness proxy:", proxy.address)
-    console.info("   > WitnetRandomness implementation:", implementation)
-    console.info("   > WitnetRandomnessImplementation:", randomness.address, `(v${await randomness.version()})`)
-    if (
-      isDryRun ||
-        ["y", "yes"].includes((await utils.prompt("   > Upgrade the proxy ? [y/N] ")).toLowerCase().trim())
-    ) {
-      try {
-        var tx = await proxy.upgradeTo(randomness.address, "0x", { from })
-        console.info("   => transaction hash :", tx.receipt.transactionHash)
-        console.info("   => transaction gas  :", tx.receipt.gasUsed)
-        console.info("   > Done.")
-      } catch (ex) {
-        console.info("   !! Cannot upgrade the proxy:")
-        console.info(ex)
+      addresses[ecosystem][network].WitnetRandomness = proxy.address
+      if (!isDryRun) {
+        utils.saveAddresses(addresses)
       }
     } else {
-      console.info("   > Not upgraded.")
+      proxy = await WitnetRandomness.at(addresses[ecosystem][network].WitnetRandomness)
+      console.info(`   Skipped: 'WitnetRandomness' deployed at ${proxy.address}`)
+    }
+
+    var randomness
+    if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetRandomnessImplementation)) {
+      await deployer.deploy(
+        WitnetRandomnessImplementation,
+        WitnetRequestBoard.address,
+        true,
+        utils.fromAscii(packageJson.version),
+        { from }
+      )
+      randomness = await WitnetRandomnessImplementation.deployed()
+      addresses[ecosystem][network].WitnetRandomnessImplementation = randomness.address
+      if (!isDryRun) {
+        utils.saveAddresses(addresses)
+      }
+    } else {
+      randomness = await WitnetRandomnessImplementation.at(addresses[ecosystem][network].WitnetRandomnessImplementation)
+      console.info(`   Skipped: 'WitnetRandomnessImplementation' deployed at ${randomness.address}`)
+    }
+
+    // console.log("proxy address =>", proxy.address)
+    // console.log(await proxy.implementation.call())
+    const implementation = await proxy.implementation()
+    if (implementation.toLowerCase() !== randomness.address.toLowerCase()) {
+      console.info()
+      console.info("   > WitnetRandomness proxy:", proxy.address)
+      console.info("   > WitnetRandomness implementation:", implementation)
+      console.info("   > WitnetRandomnessImplementation:", randomness.address, `(v${await randomness.version()})`)
+      if (
+        isDryRun ||
+          ["y", "yes"].includes((await utils.prompt("   > Upgrade the proxy ? [y/N] ")).toLowerCase().trim())
+      ) {
+        try {
+          var tx = await proxy.upgradeTo(randomness.address, "0x", { from })
+          console.info("   => transaction hash :", tx.receipt.transactionHash)
+          console.info("   => transaction gas  :", tx.receipt.gasUsed)
+          console.info("   > Done.")
+        } catch (ex) {
+          console.info("   !! Cannot upgrade the proxy:")
+          console.info(ex)
+        }
+      } else {
+        console.info("   > Not upgraded.")
+      }
+    }
+    WitnetRandomnessImplementation.address = proxy.address
+  } else {
+    if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetRandomness)) {
+      // deploy unproxified WitnetRandomness contract
+      await deployer.deploy(
+        WitnetRandomnessImplementation,
+        WitnetRequestBoard.address,
+        false,
+        utils.fromAscii(packageJson.version),
+        { from }
+      )
+      addresses[ecosystem][network].WitnetRandomness = WitnetRandomnessImplementation.address;
+      if (!isDryRun) {
+        utils.saveAddresses(addresses)
+      }
+    } else {
+      WitnetRandomnessImplementation.address = addresses[ecosystem][network]?.WitnetRandomness
+      console.info(`   Skipped: unproxied 'WitnetRandomness' deployed at ${WitnetRandomnessImplementation.address}`)
     }
   }
-  WitnetRandomnessImplementation.address = proxy.address
 }
--- migrations/scripts/7_WitnetPriceRouter.js
+++ migrations/scripts/7_WitnetPriceRouter.js
@@ -24,95 +24,114 @@ module.exports = async function (deployer, network, [, from]) {
     return;
   }
 
-  let proxy
-  if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetPriceRouter)) {
-    var create2Factory = await Create2Factory.deployed()
-    if(
-      create2Factory && !utils.isNullAddress(create2Factory.address)
-        && singletons?.WitnetPriceRouter
-    ) {
-      // Deploy the proxy via a singleton factory and a salt...
-      const bytecode = WitnetPriceRouter.toJSON().bytecode
-      const salt = singletons.WitnetPriceRouter?.salt 
-        ? "0x" + ethUtils.setLengthLeft(
-            ethUtils.toBuffer(
-              singletons.WitnetPriceRouter.salt
-            ), 32
-          ).toString("hex")
-        : "0x0"
-      ;
-      const proxyAddr = await create2Factory.determineAddr.call(bytecode, salt, { from })
-      if ((await web3.eth.getCode(proxyAddr)).length <= 3) {
-        // deploy instance only if not found in current network:
-        utils.traceHeader(`Singleton inception of 'WitnetPriceRouter':`)
-        const balance = await web3.eth.getBalance(from)
-        const gas = singletons.WitnetPriceRouter.gas || 10 ** 6
-        const tx = await create2Factory.deploy(bytecode, salt, { from, gas })
-        utils.traceTx(
-          tx.receipt,
-          web3.utils.fromWei((balance - await web3.eth.getBalance(from)).toString())
-        )
+  if (addresses[ecosystem][network]?.WitnetPriceRouterImplementation != undefined) {
+    let proxy
+    if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetPriceRouter)) {
+      var create2Factory = await Create2Factory.deployed()
+      if(
+        create2Factory && !utils.isNullAddress(create2Factory.address)
+          && singletons?.WitnetPriceRouter
+      ) {
+        // Deploy the proxy via a singleton factory and a salt...
+        const bytecode = WitnetPriceRouter.toJSON().bytecode
+        const salt = singletons.WitnetPriceRouter?.salt 
+          ? "0x" + ethUtils.setLengthLeft(
+              ethUtils.toBuffer(
+                singletons.WitnetPriceRouter.salt
+              ), 32
+            ).toString("hex")
+          : "0x0"
+        ;
+        const proxyAddr = await create2Factory.determineAddr.call(bytecode, salt, { from })
+        if ((await web3.eth.getCode(proxyAddr)).length <= 3) {
+          // deploy instance only if not found in current network:
+          utils.traceHeader(`Singleton inception of 'WitnetPriceRouter':`)
+          const balance = await web3.eth.getBalance(from)
+          const gas = singletons.WitnetPriceRouter.gas || 10 ** 6
+          const tx = await create2Factory.deploy(bytecode, salt, { from, gas })
+          utils.traceTx(
+            tx.receipt,
+            web3.utils.fromWei((balance - await web3.eth.getBalance(from)).toString())
+          )
+        } else {
+          utils.traceHeader(`Singleton 'WitnetPriceRouter':`)
+        }
+        console.info("  ", "> proxy address:       ", proxyAddr)
+        console.info("  ", "> proxy codehash:      ", web3.utils.soliditySha3(await web3.eth.getCode(proxyAddr)))        
+        console.info("  ", "> proxy inception salt:", salt)
+        proxy = await WitnetPriceRouter.at(proxyAddr)
       } else {
-        utils.traceHeader(`Singleton 'WitnetPriceRouter':`)
+        // Deploy no singleton proxy ...
+        await deployer.deploy(WitnetPriceRouter, { from })
+        proxy = await WitnetPriceRouter.deployed()
       }
-      console.info("  ", "> proxy address:       ", proxyAddr)
-      console.info("  ", "> proxy codehash:      ", web3.utils.soliditySha3(await web3.eth.getCode(proxyAddr)))        
-      console.info("  ", "> proxy inception salt:", salt)
-      proxy = await WitnetPriceRouter.at(proxyAddr)
-    } else {
-      // Deploy no singleton proxy ...
-      await deployer.deploy(WitnetPriceRouter, { from })
-      proxy = await WitnetPriceRouter.deployed()
-    }
-    addresses[ecosystem][network].WitnetPriceRouter = proxy.address
-    if (!isDryRun) {
-      utils.saveAddresses(addresses)
-    }
-  } else {
-    proxy = await WitnetPriceRouter.at(addresses[ecosystem][network].WitnetPriceRouter)
-    console.info(`   Skipped: 'WitnetPriceRouter' deployed at ${proxy.address}`)
-  }
-
-  var router
-  if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetPriceRouterImplementation)) {
-    await deployer.deploy(
-      WitnetPriceRouterImplementation,
-      true,
-      utils.fromAscii(packageJson.version),
-      { from }
-    )
-    router = await WitnetPriceRouterImplementation.deployed()
-    addresses[ecosystem][network].WitnetPriceRouterImplementation = router.address
-    if (!isDryRun) {
-      utils.saveAddresses(addresses)
-    }
-  } else {
-    router = await WitnetPriceRouterImplementation.at(addresses[ecosystem][network].WitnetPriceRouterImplementation)
-    console.info(`   Skipped: 'WitnetPriceRouterImplementation' deployed at ${router.address}`)
-  }
-
-  const implementation = await proxy.implementation()
-  if (implementation.toLowerCase() !== router.address.toLowerCase()) {
-    console.info()
-    console.info("   > WitnetPriceRouter proxy:", proxy.address)
-    console.info("   > WitnetPriceRouter implementation:", implementation)
-    console.info("   > WitnetPriceRouterImplementation:", router.address, `(v${await router.version()})`)
-    if (
-      isDryRun ||
-        ["y", "yes"].includes((await utils.prompt("   > Upgrade the proxy ? [y/N] ")).toLowerCase().trim())
-    ) {
-      try { 
-        var tx = await proxy.upgradeTo(router.address, "0x", { from })
-        console.info("   => transaction hash :", tx.receipt.transactionHash)
-        console.info("   => transaction gas  :", tx.receipt.gasUsed)
-        console.info("   > Done.")
-      } catch (ex) {
-        console.info("   !! Cannot upgrade the proxy:")
-        console.info(ex)
+      addresses[ecosystem][network].WitnetPriceRouter = proxy.address
+      if (!isDryRun) {
+        utils.saveAddresses(addresses)
       }
     } else {
-      console.info("   > Not upgraded.")
+      proxy = await WitnetPriceRouter.at(addresses[ecosystem][network].WitnetPriceRouter)
+      console.info(`   Skipped: 'WitnetPriceRouter' deployed at ${proxy.address}`)
+    }
+
+    var router
+    if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetPriceRouterImplementation)) {
+      await deployer.deploy(
+        WitnetPriceRouterImplementation,
+        true,
+        utils.fromAscii(packageJson.version),
+        { from }
+      )
+      router = await WitnetPriceRouterImplementation.deployed()
+      addresses[ecosystem][network].WitnetPriceRouterImplementation = router.address
+      if (!isDryRun) {
+        utils.saveAddresses(addresses)
+      }
+    } else {
+      router = await WitnetPriceRouterImplementation.at(addresses[ecosystem][network].WitnetPriceRouterImplementation)
+      console.info(`   Skipped: 'WitnetPriceRouterImplementation' deployed at ${router.address}`)
+    }
+
+    const implementation = await proxy.implementation()
+    if (implementation.toLowerCase() !== router.address.toLowerCase()) {
+      console.info()
+      console.info("   > WitnetPriceRouter proxy:", proxy.address)
+      console.info("   > WitnetPriceRouter implementation:", implementation)
+      console.info("   > WitnetPriceRouterImplementation:", router.address, `(v${await router.version()})`)
+      if (
+        isDryRun ||
+          ["y", "yes"].includes((await utils.prompt("   > Upgrade the proxy ? [y/N] ")).toLowerCase().trim())
+      ) {
+        try { 
+          var tx = await proxy.upgradeTo(router.address, "0x", { from })
+          console.info("   => transaction hash :", tx.receipt.transactionHash)
+          console.info("   => transaction gas  :", tx.receipt.gasUsed)
+          console.info("   > Done.")
+        } catch (ex) {
+          console.info("   !! Cannot upgrade the proxy:")
+          console.info(ex)
+        }
+      } else {
+        console.info("   > Not upgraded.")
+      }
+    }
+    WitnetPriceRouterImplementation.address = proxy.address
+  } else {
+    if (utils.isNullAddress(addresses[ecosystem][network]?.WitnetPriceRouter)) {
+      // deploy unproxified WitnetPriceRouter contract
+      await deployer.deploy(
+        WitnetPriceRouterImplementation,
+        false,
+        utils.fromAscii(packageJson.version),
+        { from }
+      )
+      addresses[ecosystem][network].WitnetPriceRouter = WitnetPriceRouterImplementation.address;
+      if (!isDryRun) {
+        utils.saveAddresses(addresses)
+      }
+    } else {
+      WitnetPriceRouterImplementation.address = addresses[ecosystem][network]?.WitnetPriceRouter
+      console.info(`   Skipped: unproxied 'WitnetPriceRouter' deployed at ${WitnetPriceRouterImplementation.address}`)
     }
   }
-  WitnetPriceRouterImplementation.address = proxy.address
 }
