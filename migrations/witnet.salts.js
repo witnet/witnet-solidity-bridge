12a7c0ea7f2cb63b3b0ad9f00a9fcff580198cb2
Guillermo Díaz <guillermo@otherplane.com>
2023-08-30 10:40:27 +0200

    chore: settle worst case gas required for create2 proxy deployments

--- migrations/witnet.salts.js
+++ migrations/witnet.salts.js
@@ -1,23 +1,22 @@
 module.exports = {
   WitnetBytecodes: {
     salt: 61396704, // => solc-0.8.17: 0x0000000e3a3d22d7510B36BdC88994dab11eadc8
-  },
-  WitnetEncodingLib: {
-    salt: 14110014, // => solc-0.8.17: 0x00011B8Db4E85F6105f21f8e9e528832e429a664
-  },
-  WitnetErrorsLib: {
-    salt: 27442272, // => solc-0.8.17: 0x77711b5098Fe75DD24839F0b412c622aE4163FaB
+    gas: 1000000,
   },
   WitnetPriceFeeds: {
     salt: 125920098, // => solc-0.8.17: 0x9999999d139bdBFbF25923ba39F63bBFc7593400
+    gas: 1000000,
   },
   WitnetRandomness: {
     salt: 332121398, // => solc-0.8.17: 0x0123456fbBC59E181D76B6Fe8771953d1953B51a
+    gas: 1000000,
   },
   WitnetRequestBoard: {
     salt: 198789840, // => solc-0-8.17: 0x777777772C24e6CD34B464D1d71616C444254537
+    gas: 1000000,
   },
   WitnetRequestFactory: {
     salt: 20377356, // => solc-0.8.17: 0x1111111FDE7dC956E3d7922Bc779D9E2349Afb63
+    gas: 1000000,
   },
 }
