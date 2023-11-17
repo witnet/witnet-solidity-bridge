3acfe73ef98aa29f8f93cff508ae01c1f454f759
Guillermo Díaz <guillermo@otherplane.com>
2023-11-17 09:53:08 +0100

    feat: extend UsingWitnet use cases

---
+++ contracts/apps/UsingWitnetRequest.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./UsingWitnet.sol";
+import "../WitnetRequest.sol";
+
+abstract contract UsingWitnetRequest
+    is UsingWitnet
+{
+    WitnetRequest immutable public dataRequest;
+    
+    bytes32 immutable internal __witnetRequestRadHash;
+    uint256 immutable internal __witnetResultMaxSize;
+ 
+    constructor (WitnetRequest _witnetRequest)
+        UsingWitnet(_witnetRequest.witnet())
+    {
+        require(
+            _witnetRequest.class() == type(WitnetRequest).interfaceId,
+            "UsingWitnetRequest: uncompliant WitnetRequest"
+        );
+        dataRequest = _witnetRequest;
+        __witnetResultMaxSize = _witnetRequest.resultDataMaxSize();
+        __witnetRequestRadHash = _witnetRequest.radHash();
+    }
+
+    function _witnetEstimateBaseFee()
+        virtual internal view
+        returns (uint256)
+    {
+        return __witnet.estimateBaseFee(
+            tx.gasprice,
+            __witnetResultMaxSize
+        );
+    }
+
+    function __witnetRequestData(
+            uint256 _witnetEvmReward,
+            WitnetV2.RadonSLA calldata _witnetQuerySLA
+        )
+        virtual internal returns (uint256)
+    {
+        return __witnetRequestData(
+            _witnetEvmReward,
+            _witnetQuerySLA,
+            __witnetRequestRadHash
+        );
+    }
+
+}
---
+++ contracts/apps/UsingWitnetRequestTemplate.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./UsingWitnet.sol";
+import "../WitnetRequest.sol";
+
+abstract contract UsingWitnetRequestTemplate
+    is UsingWitnet
+{
+    WitnetRequestTemplate immutable public dataRequestTemplate;
+    
+    uint256 immutable internal __witnetResultMaxSize;
+ 
+    constructor (WitnetRequestTemplate _requestTemplate)
+        UsingWitnet(_requestTemplate.witnet())
+    {
+        require(
+            _requestTemplate.class() == type(WitnetRequestTemplate).interfaceId,
+            "UsingWitnetRequestTemplate: uncompliant WitnetRequestTemplate"
+        );
+        dataRequestTemplate = _requestTemplate;
+        __witnetResultMaxSize = _requestTemplate.resultDataMaxSize();
+    }
+
+    function _witnetBuildRadHash(string[][] memory _witnetRequestArgs)
+        internal returns (bytes32)
+    {
+        return dataRequestTemplate.verifyRadonRequest(_witnetRequestArgs);
+    }
+    
+    function _witnetBuildRequest(string[][] memory _witnetRequestArgs)
+        internal returns (WitnetRequest)
+    {
+        return WitnetRequest(dataRequestTemplate.buildRequest(_witnetRequestArgs));
+    }
+
+    function _witnetEstimateBaseFee()
+        internal view
+        returns (uint256)
+    {
+        return __witnet.estimateBaseFee(
+            tx.gasprice,
+            __witnetResultMaxSize
+        );
+    }
+
+    function __witnetRequestData(
+            uint256 _witnetEvmReward,
+            WitnetV2.RadonSLA calldata _witnetQuerySLA,
+            string[][] memory _witnetRequestArgs
+        )
+        virtual internal returns (uint256)
+    {
+        return __witnetRequestData(
+            _witnetEvmReward,
+            _witnetQuerySLA,
+            _witnetBuildRadHash(_witnetRequestArgs)
+        );
+    }
+
+}
