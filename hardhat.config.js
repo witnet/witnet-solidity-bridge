295b22328c9e2ac8671d9ab32c408e92da0d6c91
Guillermo Díaz <guillermo@otherplane.com>
2024-02-13 08:22:26 +0100

    chore: prepare for hardhat verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,45 @@
+require("@nomicfoundation/hardhat-verify");
+
 const settings = require("./settings")
 const utils = require("./src/utils")
 const [, target] = utils.getRealmNetworkFromArgs()
+
 module.exports = {
+    networks: Object.fromEntries(
+        Object.entries(settings.getNetworks())
+            .map(([network, config]) => {
+                return [network, {
+                    chainId: config.network_id,
+                    url: `http://${config?.host || "localhost"}:${config?.port || 8545}`
+                }]
+            })
+        ),
     solidity: settings.getCompilers(target),
+    sourcify: {
+        enabled: true,
+        apiUrl: "https://sourcify.dev/server",
+        browserUrl: "https://repo.sourcify.dev",
+    },
+    etherscan: {
+        apiKey: Object.fromEntries(
+            Object.entries(settings.getNetworks())
+                .filter(([,config]) => config?.verify)
+                .map(([network, config]) => {
+                    const [ecosystem,] = utils.getRealmNetworkFromString(network)
+                    return [network, config?.verify?.apiKey ?? `ETHERSCAN_${ecosystem.toUpperCase()}_API_KEY`]
+                }),
+        ),      
+        customChains: Object.entries(settings.getNetworks())
+            .filter(([,config]) => config?.verify)
+            .map(([network, config]) => {
+                return {
+                    network,
+                    chainId: config.network_id,
+                    urls: {
+                        apiURL: config?.apiUrl,
+                        browserURL: config?.browserUrl
+                    }
+                }
+            }),
+    }
 };
