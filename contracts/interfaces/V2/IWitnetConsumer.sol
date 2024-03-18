27361c053399a693baffdbc30fb6bed24173553e
Guillermo Díaz <guillermo@otherplane.com>
2024-03-18 14:48:31 +0100

    chore: import interfaces to bypass WRB to

---
+++ contracts/interfaces/V2/IWitnetConsumer.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../../libs/Witnet.sol";
+
+interface IWitnetConsumer {
+
+    /// @notice Method to be called from the WitnetOracle contract as soon as the given Witnet `queryId`
+    /// @notice gets reported, if reported with no errors.
+    /// @dev It should revert if called from any other address different to the WitnetOracle being used
+    /// @dev by the WitnetConsumer contract. 
+    /// @param witnetQueryId The unique identifier of the Witnet query being reported.
+    /// @param witnetResultTallyHash Hash of the commit/reveal witnessing act that took place in the Witnet blockahin.
+    /// @param witnetResultTimestamp Timestamp at which the reported value was captured by the Witnet blockchain. 
+    /// @param witnetEvmFinalityBlock EVM block at which the provided data can be considered to be final.
+    /// @param witnetResultCborValue The CBOR-encoded resulting value of the Witnet query being reported.
+    function reportWitnetQueryResult(
+            uint256 witnetQueryId, 
+            uint64  witnetResultTimestamp,
+            bytes32 witnetResultTallyHash,
+            uint256 witnetEvmFinalityBlock,
+            WitnetCBOR.CBOR calldata witnetResultCborValue
+        ) external;
+
+    /// @notice Method to be called from the WitnetOracle contract as soon as the given Witnet `queryId`
+    /// @notice gets reported, if reported WITH errors.
+    /// @dev It should revert if called from any other address different to the WitnetOracle being used
+    /// @dev by the WitnetConsumer contract. 
+    /// @param witnetQueryId The unique identifier of the Witnet query being reported.
+    /// @param witnetResultTallyHash Hash of the commit/reveal witnessing act that took place in the Witnet blockahin.
+    /// @param witnetResultTimestamp Timestamp at which the reported value was captured by the Witnet blockchain. 
+    /// @param witnetEvmFinalityBlock EVM block at which the provided data can be considered to be final.
+    /// @param errorCode The error code enum identifying the error produced during resolution on the Witnet blockchain.
+    /// @param errorArgs Error arguments, if any. An empty buffer is to be passed if no error arguments apply.
+    function reportWitnetQueryError(
+            uint256 witnetQueryId, 
+            uint64  witnetResultTimestamp,
+            bytes32 witnetResultTallyHash,
+            uint256 witnetEvmFinalityBlock,
+            Witnet.ResultErrorCodes errorCode, 
+            WitnetCBOR.CBOR calldata errorArgs
+        ) external;
+
+    /// @notice Determines if Witnet queries can be reported from given address.
+    /// @dev In practice, must only be true on the WitnetOracle address that's being used by
+    /// @dev the WitnetConsumer to post queries. 
+    function reportableFrom(address) external view returns (bool);
+}
---
+++ contracts/interfaces/V2/IWitnetOracleEvents.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "../../libs/WitnetV2.sol";
+
+interface IWitnetOracleEvents {
+    
+    /// Emitted every time a new query containing some verified data request is posted to the WRB.
+    event WitnetQuery(
+        uint256 indexed id, 
+        uint256 evmReward,
+        WitnetV2.RadonSLA witnetSLA
+    );
+
+    /// Emitted when a query with no callback gets reported into the WRB.
+    event WitnetQueryResponse(
+        uint256 id, 
+        uint256 evmGasPrice
+    );
+
+    /// Emitted when a query with a callback gets successfully reported into the WRB.
+    event WitnetQueryResponseDelivered(
+        uint256 indexed id, 
+        uint256 evmGasPrice, 
+        uint256 evmCallbackGas
+    );
+
+    /// Emitted when a query with a callback cannot get reported into the WRB.
+    event WitnetQueryResponseDeliveryFailed(
+        uint256 indexed id, 
+        bytes   resultCborBytes,
+        uint256 evmGasPrice, 
+        uint256 evmCallbackActualGas, 
+        string  evmCallbackRevertReason
+    );
+
+    /// Emitted when the reward of some not-yet reported query is upgraded.
+    event WitnetQueryRewardUpgraded(
+        uint256 indexed id, 
+        uint256 evmReward
+    );
+
+}
