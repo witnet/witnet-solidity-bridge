2b35d0b32ffb15bd08eb0ccd80822e4178730366
Guillermo Díaz <guillermo@otherplane.com>
2024-08-09 12:18:20 +0200

    refactor: UsingWitOracleRandomness -> UsingWitOracle

--- contracts/mockups/UsingWitRandomness.sol
+++ contracts/mockups/UsingWitRandomness.sol
@@ -5,10 +5,10 @@ pragma experimental ABIEncoderV2;
 
 import "../WitRandomness.sol";
 
-/// @title The UsingWitOracleRandomness contract
+/// @title The UsingWitRandomness contract
 /// @dev Contracts willing to interact with WitRandomness appliance should just inherit from this contract.
 /// @author The Witnet Foundation.
-abstract contract UsingWitOracleRandomness
+abstract contract UsingWitRandomness
     is
         IWitOracleEvents,
         IWitRandomnessEvents
@@ -19,7 +19,7 @@ abstract contract UsingWitOracleRandomness
         require(
             address(_witOracleRandomness).code.length > 0
                 && _witOracleRandomness.specs() == type(WitRandomness).interfaceId,
-            "UsingWitOracleRandomness: uncompliant WitRandomness appliance"
+            "UsingWitRandomness: uncompliant WitRandomness appliance"
         );
         witOracleRandomness = _witOracleRandomness;
     }
