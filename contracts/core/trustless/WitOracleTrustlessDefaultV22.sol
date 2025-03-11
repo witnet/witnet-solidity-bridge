f596c8d52a814446abfc605c0a3691ea10c61041
Guillermo Díaz <guillermo@otherplane.com>
2025-03-11 17:25:53 +0100

    chore: postpone block relaying workflow to v2.2

--- contracts/core/trustless/WitOracleTrustlessDefaultV22.sol
+++ contracts/core/trustless/WitOracleTrustlessDefaultV22.sol
@@ -2,19 +2,19 @@
 
 pragma solidity >=0.8.0 <0.9.0;
 
-import "../base/WitOracleBaseTrustless.sol";
+import "../base/WitOracleBaseQueriableTrustless.sol";
 
 /// @title Witnet Request Board "trustless" implementation contract for regular EVM-compatible chains.
 /// @notice Contract to bridge requests to Witnet Decentralized Oracle Network.
 /// @dev This contract enables posting requests that Witnet bridges will insert into the Witnet network.
 /// The result of the requests will be posted back to this contract by the bridge nodes too.
 /// @author The Witnet Foundation
-contract WitOracleTrustlessDefaultV21
+contract WitOracleTrustlessDefaultV22
     is 
-        WitOracleBaseTrustless
+        WitOracleBaseQueriableTrustless
 {
     function class() virtual override public view  returns (string memory) {
-        return type(WitOracleTrustlessDefaultV21).name;
+        return type(WitOracleTrustlessDefaultV22).name;
     }
 
     constructor(
@@ -23,11 +23,11 @@ contract WitOracleTrustlessDefaultV21
             uint256 _queryReportingStake,
             WitOracleRadonRegistry _registry
         )
-        WitOracleBase(
+        WitOracleBaseQueriable(
             _immutables,
             _registry
         )
-        WitOracleBaseTrustless(
+        WitOracleBaseQueriableTrustless(
             _queryAwaitingBlocks,
             _queryReportingStake
         )
