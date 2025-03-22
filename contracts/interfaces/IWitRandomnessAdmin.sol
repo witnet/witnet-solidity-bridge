074b0d45d701c2f581ca65edebaeb39d0c643a72
Guillermo Díaz <guillermo@otherplane.com>
2025-03-22 11:28:58 +0100

    feat: improving randomness v2.1

--- contracts/interfaces/IWitRandomnessAdmin.sol
+++ contracts/interfaces/IWitRandomnessAdmin.sol
@@ -5,11 +5,17 @@ pragma solidity >=0.8.0 <0.9.0;
 import "../libs/Witnet.sol";
 
 interface IWitRandomnessAdmin {
+    struct WitParams {
+        uint16 maxWitCommitteeSize;
+        uint16 minWitCommitteeSize;
+        uint64 minWitInclusionFees;
+    }
     function acceptOwnership() external;
     function baseFeeOverheadPercentage() external view returns (uint16);
     function owner() external view returns (address);
     function pendingOwner() external returns (address);
-    function transferOwnership(address) external;
     function settleBaseFeeOverheadPercentage(uint16) external;
-    function settleDefaultQuerySLA(Witnet.QuerySLA calldata) external;
+    function settleWitOracleRequiredParams(WitParams calldata) external;
+    function transferOwnership(address) external;
+    function witOracleRequiredParams() external view returns (WitParams memory);
 }
