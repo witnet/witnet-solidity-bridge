9e9e4fb6fedd16aaad63eabf1bddc75dd20512c9
Guillermo Díaz <guillermo@witnet.io>
2025-11-18 18:34:37 +0100

    chore/fix: github actions

--- scripts/postinstall.cjs
+++ scripts/postinstall.cjs
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
+const exec = require("node:child_process").execSync;
 const fs = require("node:fs");
 if (!fs.existsSync(".no-postinstall") && !fs.existsSync(`${process.env.INIT_CWD}/.env_witnet`)) {
 	console.info(`Copying .env_witnet file from ${process.env.INIT_CWD}...`);
 	fs.cpSync(".env_witnet", `${process.env.INIT_CWD}/.env_witnet`);
 }
+
+if (fs.existsSync("./build/contracts")) {
+	exec('sed -i -- \"/\bsourcePath\b/d\" ./build/contracts/*.json');
+}
+
